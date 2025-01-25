101cc65cec7b339f4fd456c63f268a957234cfd3
mikekks <thdalsrb79@naver.com>
2025-01-26 01:00:21 +0900

    [UNI-7] init: 프로젝트 세팅

---
+++ uniro_backend/src/main/java/com/softeer5/uniro_backend/UniroBackendApplication.java
@@ -0,0 +1,13 @@
+package com.softeer5.uniro_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniroBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniroBackendApplication.class, args);
+	}
+
+}
