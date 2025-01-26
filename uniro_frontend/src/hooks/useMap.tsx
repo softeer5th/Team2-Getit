adb2df5366cc8dc9eedff5ed5187b8f6769ee8b0
JunhyukPark(박준혁) <peterp1@inha.edu>
2025-01-26 17:38:05 +0900

    [UNI-34] chore: CI/CD workflow 만들기

--- uniro_frontend/src/hooks/useMap.tsx
+++ uniro_frontend/src/hooks/useMap.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { initializeMap } from "../map/initializer/googleMapInitializer";
 
 const useMap = () => {
