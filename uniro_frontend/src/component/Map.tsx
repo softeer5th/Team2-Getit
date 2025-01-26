8589633aedcdf022c10e2cc4165ac03bc0b83a2d
JunhyukPark(박준혁) <peterp1@inha.edu>
2025-01-26 13:03:50 +0900

    [UNI-38] feat : 구글 맵 init 메서드 생성 및  구글 맵 hook 제작

---
+++ uniro_frontend/src/component/Map.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useRef } from 'react'
+import useMap from '../hooks/useMap'
+
+const Map = () => {
+
+    const {mapRef} = useMap();
+    
+    return (
+        <div id="map" ref={mapRef} style={{ width: "100%", height: "100%" }}></div>
+    )
+}
+
+export default Map
