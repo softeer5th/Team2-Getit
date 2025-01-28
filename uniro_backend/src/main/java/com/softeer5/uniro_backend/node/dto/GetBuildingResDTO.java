8cfe9145ce270348e61bb79b649b9908ee0476c7
mikekks <thdalsrb79@naver.com>
2025-01-28 15:53:11 +0900

    [UNI-12] chore: dto 설명 추가

--- uniro_backend/src/main/java/com/softeer5/uniro_backend/node/dto/GetBuildingResDTO.java
+++ uniro_backend/src/main/java/com/softeer5/uniro_backend/node/dto/GetBuildingResDTO.java
@@ -14,18 +14,25 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class GetBuildingResDTO {
 
-	@Schema(description = "노드 id", example = "4")
+	@Schema(description = "노드 ID", example = "4")
 	private final Long nodeId;
 
-	@Schema(description = "건물 노드 좌표", example = "{\"lag\": 127.123456, \"lat\": 37.123456}")
+	@Schema(description = "건물 노드 좌표 (위도 및 경도)",
+		example = "{\"lag\": 127.123456, \"lat\": 37.123456}")
 	private final Map<String, Double> node;
 
+	@Schema(description = "건물 이름", example = "공학관")
 	private final String buildingName;
 
+	@Schema(description = "건물 이미지 URL", example = "https://example.com/images/building.jpg")
 	private final String buildingImageUrl;
 
+	@Schema(description = "건물 전화번호",
+		example = "02-123-4567")
 	private final String phoneNumber;
 
+	@Schema(description = "건물 주소",
+		example = "서울특별시 강남구 테헤란로 123")
 	private final String address;
 
 	public static GetBuildingResDTO of(Building building, Node node) {
