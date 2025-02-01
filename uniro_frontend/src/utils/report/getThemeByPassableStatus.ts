11b0d920c7ea6c0f3787535571a853f341ca6411
JunhyukPark(박준혁) <peterp1@inha.edu>
2025-02-01 23:50:07 +0900

    [UNI-21] feat : 제보 폼 관련 Utility 생성

---
+++ uniro_frontend/src/utils/report/getThemeByPassableStatus.ts
@@ -0,0 +1,20 @@
+import { PassableStatus } from "../../data/types/report";
+
+export const getThemeByPassableStatus = (status: PassableStatus): string => {
+	const orangeTheme = "border-system-orange text-system-orange bg-[#FFF7EF]";
+	const redTheme = "border-system-red text-system-red bg-[#FFF5F7]";
+	const blueTheme = "border-primary-400 text-primary-400 bg-[#F0F5FE]";
+	const whiteTheme = "bg-gray-100 border-gray-400";
+
+	switch (status) {
+		case PassableStatus.DANGER:
+			return redTheme;
+		case PassableStatus.CAUTION:
+			return orangeTheme;
+		case PassableStatus.RESTORED:
+			return blueTheme;
+		case PassableStatus.INITIAL:
+		default:
+			return whiteTheme;
+	}
+};
