cadf33dcf8cac456f7e71cd6a9bb1d987cab8149
YoonDH <114418121+dgfh0450@users.noreply.github.com>
2025-02-03 21:30:01 +0900

    [UNI-98] feat : Tanstack Query 도입 및 커스텀 Fetch 모듈 생성 (#38)
    * [UNI-102] feat : tanstack-Query 라이브러리 설치 및 Lint 추천 설정 적용
    
    * [UNI-103] feat : Custom fetch 함수 생성 및 Get 메소드 생성 및 예시
    
    * [UNI-103] feat : Post 메소드 추가 및 CustomFetch 이름 변경 => Fetch
    
    * [UNI-103] feat : GET, POST, PUT 제네릭 타입 지정
    
    * [UNI-98] feat : Lint 적용
    
    * [UNI-98] refactor : Fetch 함수 인스턴스화하여 각 메소드 export 적용

--- uniro_frontend/src/App.tsx
+++ uniro_frontend/src/App.tsx
@@ -9,20 +9,25 @@ import NavigationResultPage from "./pages/navigationResult";
 import ReportRoutePage from "./pages/reportRoute";
 import ReportForm from "./pages/reportForm";
 import ReportHazardPage from "./pages/reportHazard";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
 	return (
-		<Routes>
-			<Route path="/" element={<Demo />} />
-			<Route path="/landing" element={<LandingPage />} />
-			<Route path="/university" element={<UniversitySearchPage />} />
-			<Route path="/building" element={<BuildingSearchPage />} />
-			<Route path="/map" element={<MapPage />} />
-			<Route path="/form" element={<ReportForm />} />
-			<Route path="/result" element={<NavigationResultPage />} />
-			<Route path="/report/route" element={<ReportRoutePage />} />
-			<Route path="/report/hazard" element={<ReportHazardPage />} />
-		</Routes>
+		<QueryClientProvider client={queryClient}>
+			<Routes>
+				<Route path="/" element={<Demo />} />
+				<Route path="/landing" element={<LandingPage />} />
+				<Route path="/university" element={<UniversitySearchPage />} />
+				<Route path="/building" element={<BuildingSearchPage />} />
+				<Route path="/map" element={<MapPage />} />
+				<Route path="/form" element={<ReportForm />} />
+				<Route path="/result" element={<NavigationResultPage />} />
+        <Route path="/report/route" element={<ReportRoutePage />} />
+        <Route path="/report/hazard" element={<ReportHazardPage />} />
+			</Routes>
+		</QueryClientProvider>
 	);
 }
 
--- uniro_frontend/src/pages/demo.tsx
+++ uniro_frontend/src/pages/demo.tsx
@@ -6,19 +6,53 @@ import Map from "../component/Map";
 import RouteInput from "../components/map/routeSearchInput";
 import OriginIcon from "../assets/map/origin.svg?react";
 import DestinationIcon from "../assets/map/destination.svg?react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ReportButton from "../components/map/reportButton";
 import { CautionToggleButton, DangerToggleButton } from "../components/map/floatingButtons";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { getFetch, postFetch, putFetch } from "../utils/fetch/fetch";
+
+const getTest = () => {
+	/** https://jsonplaceholder.typicode.com/comments?postId=1 */
+	return getFetch<{ postId: string }>("/comments", {
+		postId: 1,
+	});
+};
+
+const postTest = (): Promise<{ id: string }> => {
+	return postFetch<{ id: string }, string>("/posts", { id: "test" });
+};
+
+const putTest = (): Promise<{ id: string }> => {
+	return putFetch<{ id: string }, string>("/posts/1", { id: "test" });
+};
 
 export default function Demo() {
 	const [FailModal, isFailOpen, openFail, closeFail] = useModal();
 	const [SuccessModal, isSuccessOpen, openSuccess, closeSuccess] = useModal();
 	const [destination, setDestination] = useState<string>("역사관");
 
+	const { data, status } = useQuery({
+		queryKey: ["test"],
+		queryFn: getTest,
+	});
+
+	const { data: postData, mutate: mutatePost } = useMutation<{ id: string }>({
+		mutationFn: postTest,
+	});
+
+	const { data: putData, mutate: mutatePut } = useMutation<{ id: string }>({
+		mutationFn: putTest,
+	});
+
+	useEffect(() => {
+		console.log(data);
+	}, [status]);
+
 	return (
 		<>
 			<div className="flex flex-row">
-				<div className="w-1/3 flex flex-col justify-start space-y-5 p-5 mb-5 rounded-sm border border-dashed border-[#9747FF] ">
+				<div className="w-1/4 flex flex-col justify-start space-y-5 p-5 mb-5 rounded-sm border border-dashed border-[#9747FF] ">
 					<Button onClick={openFail}>버튼</Button>
 					<Button onClick={openSuccess} variant="secondary">
 						버튼
@@ -28,39 +62,47 @@ export default function Demo() {
 					<LandingButton />
 				</div>
 
-				<div className="w-1/3 flex flex-col justify-start space-y-5 p-5 mb-5 rounded-sm border border-dashed border-[#9747FF] ">
+				<div className="w-1/4 flex flex-col justify-start space-y-5 p-5 mb-5 rounded-sm border border-dashed border-[#9747FF] ">
 					<ReportButton />
 					<div className="flex space-x-3 rounded-sm border border-dashed border-[#9747FF] p-3">
-						<DangerToggleButton onClick={() => {}} isActive={false} />
-						<DangerToggleButton onClick={() => {}} isActive={true} />
+						<DangerToggleButton onClick={() => { }} isActive={false} />
+						<DangerToggleButton onClick={() => { }} isActive={true} />
 					</div>
 
 					<div className="flex space-x-3 rounded-sm border border-dashed border-[#9747FF] p-3">
-						<CautionToggleButton onClick={() => {}} isActive={false} />
-						<CautionToggleButton onClick={() => {}} isActive={true} />
+						<CautionToggleButton onClick={() => { }} isActive={false} />
+						<CautionToggleButton onClick={() => { }} isActive={true} />
 					</div>
 				</div>
 
-				<div className="w-1/3 rounded-sm border border-dashed border-[#9747FF] flex flex-col justify-start space-y-5 p-5">
+				<div className="w-1/4 rounded-sm border border-dashed border-[#9747FF] flex flex-col justify-start space-y-5 p-5">
 					<Input
 						placeholder="우리 학교를 검색해보세요"
-						handleVoiceInput={() => {}}
+						handleVoiceInput={() => { }}
 						onLengthChange={(e: string) => {
 							console.log(e);
 						}}
 					/>
-					<RouteInput onClick={() => {}} placeholder="출발지를 입력하세요">
+					<RouteInput onClick={() => { }} placeholder="출발지를 입력하세요">
 						<OriginIcon />
 					</RouteInput>
 
 					<RouteInput
-						onClick={() => {}}
+						onClick={() => { }}
 						placeholder="도착지를 입력하세요"
 						value={destination}
 						onCancel={() => setDestination("")}
 					>
 						<DestinationIcon />
 					</RouteInput>
+				</div>
+				<div className="w-1/4 rounded-sm border border-dashed border-[#9747FF] flex flex-col justify-start space-y-5 p-5">
+					<Button onClick={() => mutatePost()}>
+						{postData?.id ? `${postData.id} : 테스트 결과` : "POST 테스트"}
+					</Button>
+					<Button onClick={() => mutatePut()}>
+						{putData?.id ? `${putData.id} : 테스트 결과` : "PUT 테스트"}
+					</Button>
 				</div>
 				<SuccessModal>
 					<p className="text-kor-body1 font-bold text-primary-500">불편한 길 제보가 완료되었습니다!</p>
