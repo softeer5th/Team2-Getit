d66a0879780ae5d71a029101d78ff85d9f54b533
dgfh0450 <dgfh0450@gmail.com>
2025-02-03 18:08:44 +0900

    [UNI-102] feat : tanstack-Query 라이브러리 설치 및 Lint 추천 설정 적용

--- uniro_frontend/eslint.config.js
+++ uniro_frontend/eslint.config.js
@@ -4,11 +4,12 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import eslintPluginPrettier from "eslint-plugin-prettier";
+import pluginQuery from "@tanstack/eslint-plugin-query";
 
 export default tseslint.config(
 	{ ignores: ["dist"] },
 	{
-		extends: [js.configs.recommended, ...tseslint.configs.recommended],
+		extends: [js.configs.recommended, ...tseslint.configs.recommended, pluginQuery.configs["flat/recommended"]],
 		files: ["**/*.{ts,tsx}"],
 		languageOptions: {
 			ecmaVersion: 2020,
