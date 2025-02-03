542483848c607827ea56cebc8b9e584d6eed0593
JunhyukPark(박준혁) <peterp1@inha.edu>
2025-02-03 20:54:51 +0900

    [UNI-30] fix : Mock Data 분리

--- uniro_admin_frontend/src/components/log/logList.tsx
+++ uniro_admin_frontend/src/components/log/logList.tsx
@@ -1,44 +1,6 @@
 import React from "react";
 import LogCard from "./logCard";
-
-const logCardData = [
-  {
-    date: "2025-10-20",
-    change: "변경 추가",
-  },
-  {
-    date: "2025-10-20",
-    change: "변경 추가",
-  },
-  {
-    date: "2025-10-20",
-    change: "변경 추가",
-  },
-  {
-    date: "2025-10-20",
-    change: "변경 추가",
-  },
-  {
-    date: "2025-10-20",
-    change: "변경 추가",
-  },
-  {
-    date: "2025-10-20",
-    change: "변경 추가",
-  },
-  {
-    date: "2025-10-20",
-    change: "변경 추가",
-  },
-  {
-    date: "2025-10-20",
-    change: "변경 추가",
-  },
-  {
-    date: "2025-10-20",
-    change: "변경 추가",
-  },
-];
+import { logCardData } from "../../data/mock/logMockData";
 
 const LogList = () => {
   return (
