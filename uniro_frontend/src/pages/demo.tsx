4f8fa92d25a4dc33fb3174dd2221ed3042400f18
dgfh0450 <dgfh0450@gmail.com>
2025-02-04 10:28:47 +0900

    [UNI-95] feat : 검색 결과 없음 컴포넌트 생성

--- uniro_frontend/src/pages/demo.tsx
+++ uniro_frontend/src/pages/demo.tsx
@@ -11,6 +11,7 @@ import ReportButton from "../components/map/reportButton";
 import { CautionToggleButton, DangerToggleButton } from "../components/map/floatingButtons";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { getFetch, postFetch, putFetch } from "../utils/fetch/fetch";
+import SearchNull from "../components/error/SearchNull";
 
 const getTest = () => {
 	/** https://jsonplaceholder.typicode.com/comments?postId=1 */
@@ -51,8 +52,8 @@ export default function Demo() {
 
 	return (
 		<>
-			<div className="flex flex-row">
-				<div className="w-1/4 flex flex-col justify-start space-y-5 p-5 mb-5 rounded-sm border border-dashed border-[#9747FF] ">
+			<div className="flex flex-row flex-wrap">
+				<div className="w-1/3 flex flex-col justify-start space-y-5 p-5 mb-5 rounded-sm border border-dashed border-[#9747FF] ">
 					<Button onClick={openFail}>버튼</Button>
 					<Button onClick={openSuccess} variant="secondary">
 						버튼
@@ -62,7 +63,7 @@ export default function Demo() {
 					<LandingButton />
 				</div>
 
-				<div className="w-1/4 flex flex-col justify-start space-y-5 p-5 mb-5 rounded-sm border border-dashed border-[#9747FF] ">
+				<div className="w-1/3 flex flex-col justify-start space-y-5 p-5 mb-5 rounded-sm border border-dashed border-[#9747FF] ">
 					<ReportButton />
 					<div className="flex space-x-3 rounded-sm border border-dashed border-[#9747FF] p-3">
 						<DangerToggleButton onClick={() => { }} isActive={false} />
@@ -75,7 +76,7 @@ export default function Demo() {
 					</div>
 				</div>
 
-				<div className="w-1/4 rounded-sm border border-dashed border-[#9747FF] flex flex-col justify-start space-y-5 p-5">
+				<div className="w-1/3 rounded-sm border border-dashed border-[#9747FF] flex flex-col justify-start space-y-5 p-5">
 					<Input
 						placeholder="우리 학교를 검색해보세요"
 						handleVoiceInput={() => { }}
@@ -96,14 +97,6 @@ export default function Demo() {
 						<DestinationIcon />
 					</RouteInput>
 				</div>
-				<div className="w-1/4 rounded-sm border border-dashed border-[#9747FF] flex flex-col justify-start space-y-5 p-5">
-					<Button onClick={() => mutatePost()}>
-						{postData?.id ? `${postData.id} : 테스트 결과` : "POST 테스트"}
-					</Button>
-					<Button onClick={() => mutatePut()}>
-						{putData?.id ? `${putData.id} : 테스트 결과` : "PUT 테스트"}
-					</Button>
-				</div>
 				<SuccessModal>
 					<p className="text-kor-body1 font-bold text-primary-500">불편한 길 제보가 완료되었습니다!</p>
 					<div className="space-y-0">
@@ -122,9 +115,21 @@ export default function Demo() {
 						<p className="text-kor-body3 font-regular text-gray-700">다른 건물을 시도해주세요.</p>
 					</div>
 				</FailModal>
-			</div>
-			<div className="h-[500px] w-[500px]">
-				<Map />
+				<div className="w-1/2 h-[500px] rounded-sm border border-dashed border-[#9747FF]">
+					<Map />
+				</div>
+				<div className="w-1/2 rounded-sm border border-dashed border-[#9747FF] flex flex-col justify-start space-y-5 p-5">
+					<Button onClick={() => mutatePost()}>
+						{postData?.id ? `${postData.id} : 테스트 결과` : "POST 테스트"}
+					</Button>
+					<Button onClick={() => mutatePut()}>
+						{putData?.id ? `${putData.id} : 테스트 결과` : "PUT 테스트"}
+					</Button>
+					<div className="w-fit h-fit rounded-sm border border-dashed border-[#9747FF] p-5">
+						<SearchNull message="캠퍼스 내 건물명을 입력해 보세요." />
+						<SearchNull message="캠퍼스 리스트를 확인해 보세요." />
+					</div>
+				</div>
 			</div>
 			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
 		</>
