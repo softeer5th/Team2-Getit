0c87010d6e8e8449ce0a42639cb3931485823106
dgfh0450 <dgfh0450@gmail.com>
2025-02-04 10:10:26 +0900

    [UNI-104] feat : document title 변경, favicon 추가

--- uniro_frontend/vite.config.ts
+++ uniro_frontend/vite.config.ts
@@ -6,4 +6,8 @@ import svgr from "vite-plugin-svgr";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss(), svgr()],
+	server: {
+		port: 3000,
+		host: "0.0.0.0",
+	},
 });
