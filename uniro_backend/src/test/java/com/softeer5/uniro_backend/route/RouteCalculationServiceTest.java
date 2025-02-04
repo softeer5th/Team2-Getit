31882b7646de8a72770e27fa9e9151a4ed187053
mikekks <thdalsrb79@naver.com>
2025-02-04 15:03:34 +0900

    [UNI-22] test: 경로 추가 기능 관련 테스트 작성

--- uniro_backend/src/test/java/com/softeer5/uniro_backend/route/RouteCalculationServiceTest.java
+++ uniro_backend/src/test/java/com/softeer5/uniro_backend/route/RouteCalculationServiceTest.java
@@ -4,12 +4,15 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.util.List;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.softeer5.uniro_backend.common.exception.custom.RouteCalculationException;
 import com.softeer5.uniro_backend.common.exception.custom.SameStartAndEndPointException;
 import com.softeer5.uniro_backend.fixture.NodeFixture;
 import com.softeer5.uniro_backend.fixture.RouteFixture;
@@ -31,103 +34,152 @@ class RouteCalculationServiceTest {
 	private RouteRepository routeRepository;
 	@Autowired
 	private NodeRepository nodeRepository;
-	@Test
-	void 기본_경로_생성() {
-		// Given
-		Long univId = 1001L;
-		List<CreateRouteServiceReqDTO> requests = List.of(
-			new CreateRouteServiceReqDTO(0, 0),
-			new CreateRouteServiceReqDTO(1, 1),
-			new CreateRouteServiceReqDTO(2, 2)
-		);
 
-		Node node1 = NodeFixture.createNode(0, 0);
-		Node node2 = NodeFixture.createNode(1, 0);
-		Route route = RouteFixture.createRoute(node1, node2);
+	@Nested
+	class 경로추가{
+		@Test
+		void 기본_경로_생성() {
+			// Given
+			Long univId = 1001L;
+			List<CreateRouteServiceReqDTO> requests = List.of(
+				new CreateRouteServiceReqDTO(0, 0),
+				new CreateRouteServiceReqDTO(1, 1),
+				new CreateRouteServiceReqDTO(2, 2)
+			);
 
-		nodeRepository.saveAll(List.of(node1, node2));
-		routeRepository.save(route);
+			Node node0 = NodeFixture.createNode(-1, 0);
+			Node node1 = NodeFixture.createNode(0, 0);
+			Node node2 = NodeFixture.createNode(1, 0);
 
-		// When
-		List<Node> result = routeCalculationService.checkRouteCross(univId, requests);
+			Route route0 = RouteFixture.createRoute(node0, node1);
+			Route route = RouteFixture.createRoute(node1, node2);
 
-		// Then
-		assertThat(result).hasSize(3);
-		assertThat(result.get(0).isCore()).isTrue();
+			nodeRepository.saveAll(List.of(node0, node1, node2));
+			routeRepository.saveAll(List.of(route0, route));
+
+			// When
+			List<Node> result = routeCalculationService.checkRouteCross(univId, node1.getId(), null, requests);
+
+			// Then
+			assertThat(result).hasSize(3);
+			assertThat(result.get(0).isCore()).isTrue();
+		}
+
+		@Test
+		@DisplayName("wiki 페이지 TC.2")
+		void 기존_경로와_겹치는_경우_예외발생() {
+			// Given
+			Long univId = 1001L;
+			List<CreateRouteServiceReqDTO> requests = List.of(
+				new CreateRouteServiceReqDTO(0, 0),
+				new CreateRouteServiceReqDTO(1, 1),
+				new CreateRouteServiceReqDTO(2, 2)
+			);
+
+			Node node1 = NodeFixture.createNode(0, 0);
+			Node node2 = NodeFixture.createNode(1.5, 1.5);
+			Route route = RouteFixture.createRoute(node1, node2);
+
+			nodeRepository.saveAll(List.of(node1, node2));
+			routeRepository.save(route);
+
+			// when, then
+			assertThatThrownBy(() -> routeCalculationService.checkRouteCross(univId, node1.getId(), null, requests))
+				.isInstanceOf(RouteCalculationException.class)
+				.hasMessageContaining("intersection is only allowed by point");
+		}
+
+		@Test
+		void 연결된_간선이_3개가_된_경우_시작노드가_서브에서_코어노드로_변경된다() {
+			// Given
+			Long univId = 1001L;
+			List<CreateRouteServiceReqDTO> requests = List.of(
+				new CreateRouteServiceReqDTO(0, 0),
+				new CreateRouteServiceReqDTO(3, 3),
+				new CreateRouteServiceReqDTO(5, 3)
+			);
+
+			Node node0 = NodeFixture.createNode(0, -1);
+			Node node1 = NodeFixture.createNode(0, 0);
+			Node node2 = NodeFixture.createNode(0, 1);
+			Node node3 = NodeFixture.createNode(0, 2);
+			Node node4 = NodeFixture.createNode(0, 3);
+
+			Route route0 = RouteFixture.createRoute(node0, node1);
+			Route route1 = RouteFixture.createRoute(node1, node2);
+			Route route2 = RouteFixture.createRoute(node2, node3);
+			Route route3 = RouteFixture.createRoute(node3, node4);
+
+			nodeRepository.saveAll(List.of(node0, node1, node2, node3, node4));
+			routeRepository.saveAll(List.of(route0, route1, route2, route3));
+
+			// When
+			List<Node> result = routeCalculationService.checkRouteCross(univId, node1.getId(), null, requests);
+
+			// Then
+			assertThat(result).hasSize(3);
+			assertThat(result.get(0).isCore()).isTrue();
+		}
+
+		@Test
+		void 출발점과_도착점이_같은_경우_예외처리() {
+			// Given
+			Long univId = 1001L;
+			List<CreateRouteServiceReqDTO> requests = List.of(
+				new CreateRouteServiceReqDTO(0, 0),
+				new CreateRouteServiceReqDTO(1, 1),
+				new CreateRouteServiceReqDTO(0, 0)
+			);
+
+			Node node1 = NodeFixture.createNode(0, 0);
+			Node node2 = NodeFixture.createNode(0, 1.5);
+			Route route = RouteFixture.createRoute(node1, node2);
+
+			nodeRepository.saveAll(List.of(node1, node2));
+			routeRepository.save(route);
+
+			// when, Then
+			assertThatThrownBy(() -> routeCalculationService.checkRouteCross(univId, node1.getId(), null, requests))
+				.isInstanceOf(SameStartAndEndPointException.class);
+
+		}
+
+		@Test
+		@DisplayName("wiki 페이지 TC.3")
+		void 기존에_존재하는_노드와_연결할_경우_코어노드가_될_수_있다() {
+			// Given
+			Long univId = 1001L;
+			List<CreateRouteServiceReqDTO> requests = List.of(
+				new CreateRouteServiceReqDTO(0, 0),
+				new CreateRouteServiceReqDTO(2, 1),
+				new CreateRouteServiceReqDTO(2, 2),
+				new CreateRouteServiceReqDTO(0, 2),
+				new CreateRouteServiceReqDTO(-1, 2)
+			);
+
+			Node node1 = NodeFixture.createNode(0, 0);
+			Node node2 = NodeFixture.createNode(0, 1);
+			Node node3 = NodeFixture.createNode(0, 2);
+			Node node3_1 = NodeFixture.createNode(0, 3);
+
+			Route route1 = RouteFixture.createRoute(node1, node2);
+			Route route2 = RouteFixture.createRoute(node2, node3);
+			Route route2_1 = RouteFixture.createRoute(node3, node3_1);
+
+
+			nodeRepository.saveAll(List.of(node1, node2, node3, node3_1));
+			routeRepository.saveAll(List.of(route1, route2, route2_1));
+
+			// When
+			List<Node> result = routeCalculationService.checkRouteCross(univId, node1.getId(), null, requests);
+
+			// Then
+			assertThat(result).hasSize(5);
+			assertThat(result.get(0).isCore()).isFalse();
+			assertThat(result.get(3).isCore()).isTrue();
+		}
+
 	}
-
-	@Test
-	void 기존_경로와_겹치는_경우_1() {
-		// Given
-		Long univId = 1001L;
-		List<CreateRouteServiceReqDTO> requests = List.of(
-			new CreateRouteServiceReqDTO(0, 0),
-			new CreateRouteServiceReqDTO(1, 1),
-			new CreateRouteServiceReqDTO(2, 2)
-		);
-
-		Node node1 = NodeFixture.createNode(0, 0);
-		Node node2 = NodeFixture.createNode(1.5, 1.5);
-		Route route = RouteFixture.createRoute(node1, node2);
-
-		nodeRepository.saveAll(List.of(node1, node2));
-		routeRepository.save(route);
-
-		// When
-		List<Node> result = routeCalculationService.checkRouteCross(univId, requests);
-
-		// Then
-		assertThat(result).hasSize(5);
-		assertThat(result.get(0).isCore()).isTrue();
-	}
-
-	@Test
-	void 기존_경로와_겹치는_교점이_무한인_경우() {
-		// Given
-		Long univId = 1001L;
-		List<CreateRouteServiceReqDTO> requests = List.of(
-			new CreateRouteServiceReqDTO(0, 0),
-			new CreateRouteServiceReqDTO(2, 2)
-		);
-
-		Node node1 = NodeFixture.createNode(0, 0);
-		Node node2 = NodeFixture.createNode(1, 1);
-		Route route = RouteFixture.createRoute(node1, node2);
-
-		nodeRepository.saveAll(List.of(node1, node2));
-		routeRepository.save(route);
-
-		// When
-		List<Node> result = routeCalculationService.checkRouteCross(univId, requests);
-
-		// Then
-		assertThat(result).hasSize(2);
-		assertThat(result.get(0).isCore()).isTrue();
-	}
-
-	@Test
-	void 출발점과_도착점이_같은_경우_예외처리() {
-		// Given
-		Long univId = 1001L;
-		List<CreateRouteServiceReqDTO> requests = List.of(
-			new CreateRouteServiceReqDTO(0, 0),
-			new CreateRouteServiceReqDTO(1, 1),
-			new CreateRouteServiceReqDTO(0, 0)
-		);
-
-		Node node1 = NodeFixture.createNode(0, 0);
-		Node node2 = NodeFixture.createNode(1.5, 1.5);
-		Route route = RouteFixture.createRoute(node1, node2);
-
-		nodeRepository.saveAll(List.of(node1, node2));
-		routeRepository.save(route);
-
-		// when, Then
-		assertThatThrownBy(() -> routeCalculationService.checkRouteCross(univId, requests))
-			.isInstanceOf(SameStartAndEndPointException.class);
-
-	}
-
 
 
 }
