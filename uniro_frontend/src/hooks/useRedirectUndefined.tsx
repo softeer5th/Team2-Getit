fb1072233c4572adcf67fb7cf888e81a6a705d53
YoonDH <114418121+dgfh0450@users.noreply.github.com>
2025-02-05 17:30:04 +0900

    [UNI-121] chore : 서비스 라우터 변경 (#54)
    * [UNI-121] chore : 랜딩, 데모 라우터 변경 및 연결 변경
    
    * [UNI-121] chore : nginx 어드민 페이지 배포 설정 수정

--- uniro_frontend/src/hooks/useRedirectUndefined.tsx
+++ uniro_frontend/src/hooks/useRedirectUndefined.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router";
 
-export default function useRedirectUndefined<T>(deps: T[], url: string = "/landing") {
+export default function useRedirectUndefined<T>(deps: T[], url: string = "/") {
 	const navigate = useNavigate();
 
 	useEffect(() => {
