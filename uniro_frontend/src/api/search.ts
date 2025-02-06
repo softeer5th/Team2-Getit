f2f66ece6d05811c9ecc0ebeb27d06686c6e6d1c
dgfh0450 <dgfh0450@gmail.com>
2025-02-06 15:51:43 +0900

    [UNI-124] fix : 대학 리스트 조회 API Transform 패턴 타입 오류 수정

--- uniro_frontend/src/api/search.ts
+++ uniro_frontend/src/api/search.ts
@@ -1,9 +1,8 @@
 import { University } from "../data/types/university";
 import { getFetch } from "../utils/fetch/fetch";
 import { transformGetUniversityList } from "./transformer/search";
+import { GetUniversityListResponse } from "./type/response/search";
 
 export const getUniversityList = (): Promise<University[]> => {
-	return getFetch<{ data: University[]; nextCursor: number | null; hasNext: boolean }>("/univ/search").then(
-		transformGetUniversityList,
-	);
+	return getFetch<GetUniversityListResponse>("/univ/search").then(transformGetUniversityList);
 };
