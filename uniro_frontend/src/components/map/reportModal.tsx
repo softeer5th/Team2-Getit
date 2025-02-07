b99c090be58d4518bd4e5ef1ea61f2f9429f605c
dgfh0450 <dgfh0450@gmail.com>
2025-02-07 15:51:09 +0900

    [UNI-148] fix : 라우터 연결 오류 수정

--- uniro_frontend/src/components/map/reportModal.tsx
+++ uniro_frontend/src/components/map/reportModal.tsx
@@ -20,7 +20,7 @@ export default function ReportModal({ close }: ReportModalProps) {
 			</Link>
 
 			<Link
-				to={"/report/hazard"}
+				to={"/report/risk"}
 				className="w-full max-w-[418px] h-[58px] flex items-center justify-between px-5 bg-gray-100 rounded-400 text-kor-body2 font-semibold text-primary-500"
 			>
 				<p>불편한 길 제보</p>
