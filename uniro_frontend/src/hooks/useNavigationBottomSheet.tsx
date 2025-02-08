9bf928718a6945704a9d032b188587b045cd2bf6
JunhyukPark(박준혁) <peterp1@inha.edu>
2025-02-09 00:38:18 +0900

    [UNI-168] fix : constant 변수명 변경

--- uniro_frontend/src/hooks/useNavigationBottomSheet.tsx
+++ uniro_frontend/src/hooks/useNavigationBottomSheet.tsx
@@ -4,7 +4,7 @@ import { useCallback, useEffect, useRef, useState } from "react";
 const MAX_SHEET_HEIGHT = window.innerHeight * 0.7;
 const MIN_SHEET_HEIGHT = window.innerHeight * 0.35;
 const CLOSED_SHEET_HEIGHT = 0;
-const ERROR_MARGIN_OF_SCROLL = 0.95;
+const ERROR_MARGIN_OF_DRAG = 0.95;
 
 export const useNavigationBottomSheet = () => {
 	const [sheetHeight, setSheetHeight] = useState(CLOSED_SHEET_HEIGHT);
@@ -21,7 +21,7 @@ export const useNavigationBottomSheet = () => {
 
 	const preventScroll = useCallback(
 		(event: React.UIEvent<HTMLDivElement>) => {
-			if (sheetHeight < MAX_SHEET_HEIGHT * ERROR_MARGIN_OF_SCROLL) {
+			if (sheetHeight < MAX_SHEET_HEIGHT * ERROR_MARGIN_OF_DRAG) {
 				event.preventDefault();
 				if (scrollRef.current) {
 					scrollRef.current.scrollTo({ top: 0, behavior: "smooth" });
@@ -32,7 +32,7 @@ export const useNavigationBottomSheet = () => {
 	);
 	useEffect(() => {
 		if (scrollRef.current) {
-			if (sheetHeight > MAX_SHEET_HEIGHT * ERROR_MARGIN_OF_SCROLL) {
+			if (sheetHeight > MAX_SHEET_HEIGHT * ERROR_MARGIN_OF_DRAG) {
 				scrollRef.current.style.overflowY = "auto";
 			} else {
 				scrollRef.current.scrollTo({ top: 0, behavior: "smooth" });
