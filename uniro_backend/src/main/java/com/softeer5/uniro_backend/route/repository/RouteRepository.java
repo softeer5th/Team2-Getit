9890140078113c275067fee22d8ad0a3bc962e95
mikekks <thdalsrb79@naver.com>
2025-02-09 02:34:00 +0900

    [UNI-27] feat: 기존 데이터 삭제 로직 구현

--- uniro_backend/src/main/java/com/softeer5/uniro_backend/route/repository/RouteRepository.java
+++ uniro_backend/src/main/java/com/softeer5/uniro_backend/route/repository/RouteRepository.java
@@ -1,13 +1,15 @@
 package com.softeer5.uniro_backend.route.repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.softeer5.uniro_backend.route.entity.Route;
 
@@ -23,38 +25,11 @@ public interface RouteRepository extends JpaRepository<Route, Long> {
             nativeQuery = true)
     List<Route> findRiskRouteByUnivId(@Param("univId") Long univId);
 
-    @Query(value = """
-    SELECT r.* FROM route r
-    JOIN node n1 ON r.node1_id = n1.id
-    JOIN node n2 ON r.node2_id = n2.id
-    WHERE r.univ_id = :univId
-      AND (
-        (n1.coordinates = ST_SRID(ST_GeomFromText(:point1), 4326)
-        AND n2.coordinates = ST_SRID(ST_GeomFromText(:point2), 4326))
-        OR
-        (n1.coordinates = ST_SRID(ST_GeomFromText(:point2), 4326)
-        AND n2.coordinates = ST_SRID(ST_GeomFromText(:point1), 4326))
-      )
-    """, nativeQuery = true)
-    Optional<Route> findRouteByPointsAndUnivId(
-            @Param("univId") Long univId,
-            @Param("point1") String point1,
-            @Param("point2") String point2
-    );
-
-    @Query(value = """
-    SELECT r.* FROM route r
-    WHERE r.univ_id = :univId
-    AND (
-        r.path = ST_SRID(ST_GeomFromText(:path), 4326)
-        OR
-        r.path = ST_SRID(ST_GeomFromText(:rev_path), 4326)
-    )
-""", nativeQuery = true)
-    Optional<Route> findRouteByLineStringAndUnivId(@Param("univId") Long univId,
-                                                    @Param("path")String path,
-                                                    @Param("rev_path")String revPath);
-
     Optional<Route> findByIdAndUnivId (Long id, Long univId);
+
+    @Modifying(clearAutomatically = true)
+    @Transactional
+    @Query("DELETE FROM Route r WHERE r.univId =:univId AND r.createdAt > :versionTimeStamp")
+    void deleteAllByCreatedAt(@Param("univId") Long univId, @Param("versionTimeStamp") LocalDateTime versionTimeStamp);
 
 }
