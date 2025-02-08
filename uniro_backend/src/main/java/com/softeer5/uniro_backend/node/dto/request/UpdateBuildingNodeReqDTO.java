8c4dcf1c2d71bd0e91de05cb7c6aad134b24718f
songhyeonseong <shsum7@gmail.com>
2025-02-08 23:58:55 +0900

    [UNI-155] 변수명 통일작업

--- uniro_backend/src/main/java/com/softeer5/uniro_backend/node/dto/request/UpdateBuildingNodeReqDTO.java
+++ uniro_backend/src/main/java/com/softeer5/uniro_backend/node/dto/request/UpdateBuildingNodeReqDTO.java
@@ -9,13 +9,13 @@ import lombok.RequiredArgsConstructor;
 @Schema(name = "UpdateBuildingNodeReqDTO", description = "건물 노드 정보 수정 요청 DTO")
 public class UpdateBuildingNodeReqDTO {
     @Schema(description = "건물명", example = "공학관")
-    private final String name;
+    private final String buildingName;
     @Schema(description = "전화번호", example = "02-1234-1234")
     private final String phoneNumber;
     @Schema(description = "주소", example = "한양로 123번길123")
     private final String address;
     @Schema(description = "이미지", example = "")
-    private final String imageUrl; // 추후 S3학습 후 변경예정
+    private final String buildingImageUrl; // 추후 S3학습 후 변경예정
     @Schema(description = "레벨(지도 축척에 따른 노출정도, 1~10)", example = "3")
     private final int level;
 }
