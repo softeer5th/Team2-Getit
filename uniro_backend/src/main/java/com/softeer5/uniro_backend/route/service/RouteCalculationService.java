6dbdfe1237d32a024dfc3fe46875d8c68280449d
songhyeonseong <shsum7@gmail.com>
2025-02-08 10:41:12 +0900

    [UNI-160] feat : 길찾기 도중 위험한 길은 추천되지 않도록 수정

--- uniro_backend/src/main/java/com/softeer5/uniro_backend/route/service/RouteCalculationService.java
+++ uniro_backend/src/main/java/com/softeer5/uniro_backend/route/service/RouteCalculationService.java
@@ -134,6 +134,7 @@ public class RouteCalculationService {
                     && currentDistance > costMap.get(currentNode.getId())) continue;
 
             for(Route route : adjMap.getOrDefault(currentNode.getId(), Collections.emptyList())){
+                if(!route.getDangerFactors().isEmpty())continue;
                 double newDistance = currentDistance + route.getCost();
                 Node nextNode = route.getNode1().getId().equals(currentNode.getId())?route.getNode2():route.getNode1();
                 if(!costMap.containsKey(nextNode.getId()) || costMap.get(nextNode.getId()) > newDistance){
