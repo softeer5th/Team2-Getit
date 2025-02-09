340cac5bbe68b9d05bd75f693be7915159c1864d
Junhyeok Park(박준혁) <peterp1@naver.com>
2025-02-09 15:33:02 +0900

    [UNI-175] feat : Router로 페이지별 작업 환경 설정하기 (#87)

---
+++ uniro_admin_frontend/src/page/buildingPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MainContainer from "../container/mainContainer";
+
+type Props = {};
+
+const BuildingPage = (props: Props) => {
+  return (
+    <MainContainer>
+      <div>BuildingPage</div>;
+    </MainContainer>
+  );
+};
+
+export default BuildingPage;
