07cca6bed964ce06dd40b12767d80ac54b948609
dgfh0450 <dgfh0450@gmail.com>
2025-02-09 15:17:58 +0900

    [UNI-145] feat : 커스텀 ErrorBoundary 구현 및 적용

--- uniro_frontend/src/App.tsx
+++ uniro_frontend/src/App.tsx
@@ -16,6 +16,7 @@ import useNetworkStatus from "./hooks/useNetworkStatus";
 import ErrorPage from "./pages/error";
 import { Suspense } from "react";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import ErrorBoundary from "./components/error/ErrorBoundary";
 
 const queryClient = new QueryClient();
 
@@ -24,22 +25,24 @@ function App() {
 	useNetworkStatus();
 	return (
 		<QueryClientProvider client={queryClient}>
-			<Suspense key={location.key} fallback={fallback}>
-				<Routes>
-					<Route path="/demo" element={<Demo />} />
-					<Route path="/" element={<LandingPage />} />
-					<Route path="/university" element={<UniversitySearchPage />} />
-					<Route path="/building" element={<BuildingSearchPage />} />
-					<Route path="/map" element={<MapPage />} />
-					<Route path="/form" element={<ReportForm />} />
-					<Route path="/result" element={<NavigationResultPage />} />
-					<Route path="/report/route" element={<ReportRoutePage />} />
-					<Route path="/report/risk" element={<ReportRiskPage />} />
-					/** 에러 페이지 */
-					<Route path="/error" element={<ErrorPage />} />
-					<Route path="/error/offline" element={<OfflinePage />} />
-				</Routes>
-			</Suspense>
+			<ErrorBoundary fallback={<ErrorPage />}>
+				<Suspense key={location.key} fallback={fallback}>
+					<Routes>
+						<Route path="/demo" element={<Demo />} />
+						<Route path="/" element={<LandingPage />} />
+						<Route path="/university" element={<UniversitySearchPage />} />
+						<Route path="/building" element={<BuildingSearchPage />} />
+						<Route path="/map" element={<MapPage />} />
+						<Route path="/form" element={<ReportForm />} />
+						<Route path="/result" element={<NavigationResultPage />} />
+						<Route path="/report/route" element={<ReportRoutePage />} />
+						<Route path="/report/risk" element={<ReportRiskPage />} />
+						/** 에러 페이지 */
+						<Route path="/error" element={<ErrorPage />} />
+						<Route path="/error/offline" element={<OfflinePage />} />
+					</Routes>
+				</Suspense>
+			</ErrorBoundary>
 			<ReactQueryDevtools initialIsOpen={false} />
 		</QueryClientProvider>
 	);
