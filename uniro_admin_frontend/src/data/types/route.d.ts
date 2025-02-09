033ad9118f4ca5723541b9ecba5686cc885254f5
dgfh0450 <dgfh0450@gmail.com>
2025-02-09 18:34:42 +0900

    [UNI-177] feat : Coord, node, Route 타입 선언

---
+++ uniro_admin_frontend/src/data/types/route.d.ts
@@ -0,0 +1,54 @@
+import {
+  CautionIssueType,
+  DangerIssueType,
+} from "../../constant/enum/reportEnum";
+import { RoutePoint } from "../../constant/enum/routeEnum";
+import { Coord } from "./coord";
+import { Node } from "./node";
+
+export type RouteId = number;
+
+export type Route = {
+  routeId: RouteId;
+  node1: Coord;
+  node2: Coord;
+};
+
+export interface CautionRoute extends Route {
+  cautionTypes: CautionIssueType[];
+}
+
+export interface DangerRoute extends Route {
+  dangerTypes: DangerIssueType[];
+}
+
+export interface CoreRoute {
+  routeId: RouteId;
+  node1: Node;
+  node2: Node;
+}
+
+export interface CoreRoutes {
+  coreNode1Id: NodeId;
+  coreNode2Id: NodeId;
+  routes: CoreRoute[];
+}
+
+export type CoreRoutesList = CoreRoutes[];
+
+export type RoutePointType = RoutePoint.ORIGIN | RoutePoint.DESTINATION;
+
+export type RouteDetail = {
+  dist: number;
+  directionType: Direction;
+  coordinates: Coord;
+  cautionFactors: CautionIssueType[];
+};
+
+export type NavigationRouteList = {
+  hasCaution: boolean;
+  totalDistance: number;
+  totalCost: number;
+  routes: Route[];
+  routeDetails: RouteDetail[];
+};
