b214af5a6d9acac863e25dbccddbd806ab1cf35b
dgfh0450 <dgfh0450@gmail.com>
2025-02-09 18:13:00 +0900

    [UNI-177] feat : TanstackQuery devtool 설치 및 QueryClient 설정

--- uniro_admin_frontend/src/AppRouter.tsx
+++ uniro_admin_frontend/src/AppRouter.tsx
@@ -3,19 +3,26 @@ import App from "./App";
 import LogPage from "./page/logPage";
 import BuildingPage from "./page/buildingPage";
 import SimulationPage from "./page/simulationPage";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 function AppRouter() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<LogPage />} />
-          <Route path="logs" element={<LogPage />} />
-          <Route path="buildings" element={<BuildingPage />} />
-          <Route path="simulation" element={<SimulationPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route index element={<LogPage />} />
+            <Route path="logs" element={<LogPage />} />
+            <Route path="buildings" element={<BuildingPage />} />
+            <Route path="simulation" element={<SimulationPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
