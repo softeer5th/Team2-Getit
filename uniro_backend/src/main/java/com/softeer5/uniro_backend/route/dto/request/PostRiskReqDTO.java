2bd8b1d15f53c355612093ec56bfb163b70bb175
송민규 <100754581+mikekks@users.noreply.github.com>
2025-02-10 17:19:51 +0900

    [UNI-178], [UNI-179] fix : 클라이언트 요청값 검증 로직 구현 (#93)
    * [UNI-178] : fix: 클라이언트 요청 validation
    
    * [UNI-178] : fix: 빠른 길 찾기 API 에서 시작&종료 노드 검증
    
    * [UNI-178] fix: 커스텀 예외 적용
    
    * [UNI-178] fix: NotNull 조건 제외

--- uniro_backend/src/main/java/com/softeer5/uniro_backend/route/dto/request/PostRiskReqDTO.java
+++ uniro_backend/src/main/java/com/softeer5/uniro_backend/route/dto/request/PostRiskReqDTO.java
@@ -4,6 +4,7 @@ import com.softeer5.uniro_backend.route.entity.CautionFactor;
 import com.softeer5.uniro_backend.route.entity.DangerFactor;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -15,8 +16,10 @@ import java.util.List;
 public class PostRiskReqDTO {
 
     @Schema(description = "주의 요소 목록", example = "[\"SLOPE\", \"CURB\"]")
+    @NotNull
     private List<CautionFactor> cautionFactors;
 
     @Schema(description = "위험 요소 목록", example = "[\"CURB\", \"STAIRS\"]")
+    @NotNull
     private List<DangerFactor> dangerFactors;
 }
