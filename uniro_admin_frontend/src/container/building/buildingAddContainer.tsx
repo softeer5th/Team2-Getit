27cae4537621179eae8e21d56f85164e0c24ad71
JunhyukPark(박준혁) <peterp1@inha.edu>
2025-02-10 17:34:19 +0900

    [UNI-180] feat : 건물 -> 길 기능 추가 및 건물 추가 API 연결

--- uniro_admin_frontend/src/container/building/buildingAddContainer.tsx
+++ uniro_admin_frontend/src/container/building/buildingAddContainer.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { Building } from "../../data/types/node";
+import { Building, Node } from "../../data/types/node";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { postBuilding } from "../../api/nodes";
 
 type Coord = {
   lat: number;
@@ -11,6 +13,10 @@ type BuildingAddContainerProps = {
   setSelectedCoord?: React.Dispatch<React.SetStateAction<Coord | undefined>>;
   markerRef: React.MutableRefObject<google.maps.marker.AdvancedMarkerElement | null>;
   selectedBuilding: Building | null;
+  drawSingleRoute: (start: Node, end: Node) => void;
+  mode: "add" | "connect" | "view";
+  selectedNode: Node[];
+  resetConnectMode: () => void;
 };
 
 const BuildingAddContainer: React.FC<BuildingAddContainerProps> = ({
@@ -18,11 +24,36 @@ const BuildingAddContainer: React.FC<BuildingAddContainerProps> = ({
   setSelectedCoord,
   markerRef,
   selectedBuilding,
+  mode,
+  selectedNode,
+  drawSingleRoute,
+  resetConnectMode,
 }) => {
+  const queryClient = useQueryClient();
   const [buildingName, setBuildingName] = useState("");
   const [buildingPhoto, setBuildingPhoto] = useState("");
   const [phone, setPhone] = useState("");
   const [address, setAddress] = useState("");
+
+  const [isSelectedNode1, setIsSelectedNode1] = useState(true);
+
+  const addBuilding = useMutation({
+    mutationFn: (body: {
+      buildingName: string;
+      buildingImageUrl: string;
+      phoneNumber: string;
+      address: string;
+      lat: number;
+      lng: number;
+      level: number;
+    }) => postBuilding(1001, body),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [1001, "buildings"] });
+    },
+    onError: (error) => {
+      alert("건물 추가에 실패했습니다.");
+    },
+  });
 
   useEffect(() => {
     if (selectedBuilding) {
@@ -53,13 +84,19 @@ const BuildingAddContainer: React.FC<BuildingAddContainerProps> = ({
 
     const payload = {
       buildingName,
-      buildingPhoto,
-      phone,
+      buildingImageUrl: buildingPhoto,
+      phoneNumber: phone,
       address,
-      lat: selectedCoord?.lat || selectedBuilding?.lat,
-      lng: selectedCoord?.lng || selectedBuilding?.lng,
+      lat: selectedCoord?.lat ?? selectedBuilding?.lat ?? 0,
+      lng: selectedCoord?.lng ?? selectedBuilding?.lng ?? 0,
+      level: 10,
     };
-    console.log(payload);
+    if (mode === "add") {
+      if (payload.lat && payload.lng) {
+        addBuilding.mutate(payload);
+      }
+    }
+
     alert(
       selectedBuilding ? "건물이 수정되었습니다." : "건물이 추가되었습니다."
     );
@@ -74,74 +111,198 @@ const BuildingAddContainer: React.FC<BuildingAddContainerProps> = ({
 
   return (
     <div className="flex flex-col items-start w-1/5 border-x-2 border-gray-300 h-full">
-      {selectedCoord || selectedBuilding ? (
-        <>
-          {selectedCoord && (
-            <div className="text-kor-body1 flex flex-col gap-2 items-start bg-gray-100 p-2 w-full ">
-              <p className="font-semibold text-gray-700">선택한 위치:</p>
-              <p className="text-blue-500">lat: {selectedCoord.lat}</p>
-              <p className="text-green-500">lng: {selectedCoord.lng}</p>
+      {mode === "add" || mode === "view" ? (
+        selectedCoord || selectedBuilding ? (
+          <>
+            {selectedCoord && (
+              <div className="text-kor-body1 flex flex-col gap-2 items-start bg-gray-100 p-2 w-full ">
+                <p className="font-semibold text-gray-700">선택한 위치:</p>
+                <p className="text-blue-500">lat: {selectedCoord.lat}</p>
+                <p className="text-green-500">lng: {selectedCoord.lng}</p>
+              </div>
+            )}
+
+            <form
+              className="flex-1 flex flex-col items-start w-full h-full p-4"
+              onSubmit={handleSubmit}
+            >
+              <h2 className="text-kor-heading1 pb-2 border-b-2 w-full mb-2 border-gray-300 text-left font-semibold">
+                {selectedBuilding ? "건물 수정하기" : "건물 추가하기"}
+              </h2>
+
+              <div className="text-kor-body1 mb-1">건물명</div>
+              <input
+                type="text"
+                className="w-full h-10 border-2 border-gray-300 rounded-md p-2 mb-4"
+                value={buildingName}
+                onChange={(e) => setBuildingName(e.target.value)}
+                required
+              />
+
+              <div className="text-kor-body1 mb-1">건물 사진</div>
+              <input
+                type="text"
+                className="w-full h-10 border-2 border-gray-300 rounded-md p-2 mb-4"
+                value={buildingPhoto}
+                onChange={(e) => setBuildingPhoto(e.target.value)}
+                required
+              />
+
+              <div className="text-kor-body1 mb-1">전화번호</div>
+              <input
+                type="tel"
+                className="w-full h-10 border-2 border-gray-300 rounded-md p-2 mb-4"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+                required
+              />
+
+              <div className="text-kor-body1 mb-1">주소</div>
+              <input
+                type="text"
+                className="w-full h-10 border-2 border-gray-300 rounded-md p-2 mb-4"
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+                required
+              />
+
+              <div className="flex justify-end w-full">
+                <button
+                  type="submit"
+                  className="px-4 py-2 rounded-md text-sm font-semibold transition-colors 
+                bg-blue-500 text-white hover:bg-blue-600"
+                >
+                  {selectedBuilding ? "수정하기" : "추가하기"}
+                </button>
+              </div>
+            </form>
+          </>
+        ) : (
+          <div className="flex-1 flex items-center justify-center w-full h-full">
+            <div className="text-kor-heading2">
+              지도에서 위치를 선택해주세요
             </div>
+          </div>
+        )
+      ) : (
+        <div className="flex flex-col justify-start w-full h-full text-left p-4 bg-white rounded-lg shadow-md border border-gray-300">
+          <h3 className="text-lg font-semibold text-gray-700 mb-3">
+            선택된 정보
+          </h3>
+          <button
+            onClick={() => resetConnectMode()}
+            className="px-4 py-2 rounded-md text-sm font-semibold transition-colors 
+                bg-blue-500 text-white hover:bg-blue-600 mb-2"
+          >
+            초기화
+          </button>
+
+          {selectedBuilding ? (
+            <div className="p-3 bg-gray-100 rounded-md shadow-sm mb-2">
+              <p className="text-sm text-gray-900">
+                🏢 <span className="font-medium">건물명:</span>{" "}
+                {selectedBuilding.buildingName}
+              </p>
+            </div>
+          ) : (
+            <p className="text-sm text-gray-500">🏢 선택된 건물이 없습니다.</p>
           )}
 
-          <form
-            className="flex-1 flex flex-col items-start w-full h-full p-4"
-            onSubmit={handleSubmit}
-          >
-            <h2 className="text-kor-heading1 pb-2 border-b-2 w-full mb-2 border-gray-300 text-left font-semibold">
-              {selectedBuilding ? "건물 수정하기" : "건물 추가하기"}
-            </h2>
-
-            <div className="text-kor-body1 mb-1">건물명</div>
-            <input
-              type="text"
-              className="w-full h-10 border-2 border-gray-300 rounded-md p-2 mb-4"
-              value={buildingName}
-              onChange={(e) => setBuildingName(e.target.value)}
-              required
-            />
-
-            <div className="text-kor-body1 mb-1">건물 사진</div>
-            <input
-              type="text"
-              className="w-full h-10 border-2 border-gray-300 rounded-md p-2 mb-4"
-              value={buildingPhoto}
-              onChange={(e) => setBuildingPhoto(e.target.value)}
-              required
-            />
-
-            <div className="text-kor-body1 mb-1">전화번호</div>
-            <input
-              type="tel"
-              className="w-full h-10 border-2 border-gray-300 rounded-md p-2 mb-4"
-              value={phone}
-              onChange={(e) => setPhone(e.target.value)}
-              required
-            />
-
-            <div className="text-kor-body1 mb-1">주소</div>
-            <input
-              type="text"
-              className="w-full h-10 border-2 border-gray-300 rounded-md p-2 mb-4"
-              value={address}
-              onChange={(e) => setAddress(e.target.value)}
-              required
-            />
-
-            <div className="flex justify-end w-full">
-              <button
-                type="submit"
-                className="px-4 py-2 rounded-md text-sm font-semibold transition-colors 
-                bg-blue-500 text-white hover:bg-blue-600"
+          {selectedNode.length > 0 ? (
+            <>
+              <div
+                className={`flex flex-col p-3 bg-gray-100 rounded-md shadow-sm ${isSelectedNode1 && "border-2 border-blue-500"} mb-2`}
               >
-                {selectedBuilding ? "수정하기" : "추가하기"}
+                <p className="text-sm text-gray-600 font-medium">
+                  📍 선택한 노드 정보 1
+                </p>
+                <p className="text-sm text-blue-500">
+                  NODE ID: {selectedNode[0].nodeId}
+                </p>
+                <p className="text-sm text-blue-500">
+                  위도: {selectedNode[0].lat}
+                </p>
+                <p className="text-sm text-green-500">
+                  경도: {selectedNode[0].lng}
+                </p>
+                <div className="flex justify-end w-full">
+                  <button
+                    onClick={() => {
+                      drawSingleRoute(
+                        {
+                          nodeId: selectedNode[0].nodeId,
+                          lat: selectedBuilding?.lat ?? 0,
+                          lng: selectedBuilding?.lng ?? 0,
+                        },
+                        selectedNode[0]
+                      );
+                      setIsSelectedNode1(true);
+                    }}
+                    className="px-4 py-2 rounded-md text-sm font-semibold transition-colors 
+                bg-blue-500 text-white hover:bg-blue-600"
+                  >
+                    연결해보기
+                  </button>
+                </div>
+              </div>
+              <div
+                className={`flex flex-col p-3 bg-gray-100 rounded-md shadow-sm ${!isSelectedNode1 && "border-2 border-blue-500"} mb-2`}
+              >
+                <p className="text-sm text-gray-600 font-medium">
+                  📍 선택한 노드 정보 2
+                </p>
+                <p className="text-sm text-blue-500">
+                  NODE ID: {selectedNode[1].nodeId}
+                </p>
+                <p className="text-sm text-blue-500">
+                  위도: {selectedNode[1].lat}
+                </p>
+                <p className="text-sm text-green-500">
+                  경도: {selectedNode[1].lng}
+                </p>
+                <div className="flex justify-end w-full">
+                  <button
+                    onClick={() => {
+                      drawSingleRoute(
+                        {
+                          nodeId: selectedNode[0].nodeId,
+                          lat: selectedBuilding?.lat ?? 0,
+                          lng: selectedBuilding?.lng ?? 0,
+                        },
+                        selectedNode[1]
+                      );
+                      setIsSelectedNode1(false);
+                    }}
+                    className="px-4 py-2 rounded-md text-sm font-semibold transition-colors 
+                bg-blue-500 text-white hover:bg-blue-600"
+                  >
+                    연결해보기
+                  </button>
+                </div>
+              </div>
+              <button
+                onClick={() => {
+                  drawSingleRoute(
+                    {
+                      nodeId: selectedNode[0].nodeId,
+                      lat: selectedBuilding?.lat ?? 0,
+                      lng: selectedBuilding?.lng ?? 0,
+                    },
+                    selectedNode[1]
+                  );
+                  setIsSelectedNode1(false);
+                }}
+                className="px-4 py-2 rounded-md text-sm font-semibold transition-colors 
+                bg-red-500 text-white hover:bg-red-600"
+              >
+                연결하기
               </button>
-            </div>
-          </form>
-        </>
-      ) : (
-        <div className="flex-1 flex items-center justify-center w-full h-full">
-          <div className="text-kor-heading2">지도에서 위치를 선택해주세요</div>
+            </>
+          ) : (
+            <p className="text-sm text-gray-500">
+              📍 선택된 길 좌표가 없습니다.
+            </p>
+          )}
         </div>
       )}
     </div>
