a44d989374a8e4334c9a693a84bb49540073ab9f
mikekks <thdalsrb79@naver.com>
2025-02-11 10:23:08 +0900

    [UNI-27] fix: createdAt 동작을 위한 어노테이션 추가

--- uniro_backend/src/main/java/com/softeer5/uniro_backend/UniroBackendApplication.java
+++ uniro_backend/src/main/java/com/softeer5/uniro_backend/UniroBackendApplication.java
@@ -2,8 +2,10 @@ package com.softeer5.uniro_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class UniroBackendApplication {
 
 	public static void main(String[] args) {
