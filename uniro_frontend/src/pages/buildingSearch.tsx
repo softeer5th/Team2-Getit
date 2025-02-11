4bf5c9c099db6e3b52c2b6f6b37413cf87a25249
YoonDH <114418121+dgfh0450@users.noreply.github.com>
2025-02-11 10:52:14 +0900

    [UNI-190] feat : 건물 리스트 API 적용 및 Default 도착지 제거 (#103)

--- uniro_frontend/src/pages/buildingSearch.tsx
+++ uniro_frontend/src/pages/buildingSearch.tsx
@@ -4,21 +4,37 @@ import BuildingCard from "../components/building/buildingCard";
 import useSearchBuilding from "../hooks/useSearchBuilding";
 import useUniversityInfo from "../hooks/useUniversityInfo";
 import useRedirectUndefined from "../hooks/useRedirectUndefined";
+import { useQuery } from "@tanstack/react-query";
+import { getAllBuildings } from "../api/nodes";
+import { University } from "../data/types/university";
 
 export default function BuildingSearchPage() {
+	const { university } = useUniversityInfo();
 	const { setBuilding } = useSearchBuilding();
 
-	const { university } = useUniversityInfo();
-	useRedirectUndefined<string | undefined>([university]);
+	if (!university) return;
+
+	const { data: buildings } = useQuery({
+		queryKey: [university.id, "buildings"],
+		queryFn: () =>
+			getAllBuildings(university.id, {
+				leftUpLat: 38,
+				leftUpLng: 127,
+				rightDownLat: 37,
+				rightDownLng: 128,
+			}),
+	},)
+
+	useRedirectUndefined<University | undefined>([university]);
 
 	return (
 		<div className="relative flex flex-col h-dvh w-full max-w-[450px] mx-auto justify-center">
 			<div className="px-[14px] py-4 border-b-[1px] border-gray-400">
-				<Input onLengthChange={() => {}} handleVoiceInput={() => {}} placeholder="" />
+				<Input onLengthChange={() => { }} handleVoiceInput={() => { }} placeholder="" />
 			</div>
 			<div className="flex-1 overflow-y-scroll">
 				<ul className="px-4 pt-1 space-y-1">
-					{hanyangBuildings.map((building) => (
+					{(buildings ?? []).map((building) => (
 						<BuildingCard
 							onClick={() => setBuilding(building)}
 							key={`building-${building.buildingName}`}
