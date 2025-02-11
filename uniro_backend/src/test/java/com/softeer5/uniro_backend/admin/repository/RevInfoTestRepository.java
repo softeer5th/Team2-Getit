f514d3b1cc469a37b57281b3bef05e7f30c7aa09
mikekks <thdalsrb79@naver.com>
2025-02-11 20:35:17 +0900

    [UNI-27] feat: 테스트 레포지토리 구현

---
+++ uniro_backend/src/test/java/com/softeer5/uniro_backend/admin/repository/RevInfoTestRepository.java
@@ -0,0 +1,8 @@
+package com.softeer5.uniro_backend.admin.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.softeer5.uniro_backend.admin.entity.RevInfo;
+
+public interface RevInfoTestRepository extends JpaRepository<RevInfo,Long>  {
+}
