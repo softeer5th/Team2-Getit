b636ab5cc18f358b0a73dfa27066dcc6c99cd5e9
dgfh0450 <dgfh0450@gmail.com>
2025-02-11 10:51:00 +0900

    [UNI-190] feat : 건물 리스트 API 적용 및 Default 도착지 제거

--- uniro_frontend/src/hooks/useRoutePoint.ts
+++ uniro_frontend/src/hooks/useRoutePoint.ts
@@ -1,31 +1,10 @@
 import { create } from "zustand";
 import { Building } from "../data/types/node";
 
-const originMockInfo = {
-	buildingName: "5호관",
-	buildingImageUrl: "",
-	phoneNumber: "",
-	address: "인하로 100",
-	nodeId: 1,
-	lng: 127.042012,
-	lat: 37.557643,
-};
-
-const destMockInfo = {
-	buildingName: "하이테크",
-	buildingImageUrl: "",
-	phoneNumber: "",
-	address: "인하로 100",
-	nodeId: 2,
-	lng: 127.042012,
-	lat: 37.557643,
-};
-
 interface RouteStore {
-	origin: Building;
+	origin: Building | undefined;
 	setOrigin: (origin: Building | undefined) => void;
-	destination: Building;
-	setDemoBuildingInfo: (building: Building) => void;
+	destination: Building | undefined;
 	setDestination: (destination: Building | undefined) => void;
 	setOriginCoord: (lng: number, lat: number) => void;
 	setDestinationCoord: (lng: number, lat: number) => void;
@@ -34,9 +13,9 @@ interface RouteStore {
 
 /** 출발지, 도착지 관리 전역 상태 */
 const useRoutePoint = create<RouteStore>((set) => ({
-	origin: originMockInfo,
+	origin: undefined,
 	setOrigin: (newOrigin: Building | undefined) => set(() => ({ origin: newOrigin })),
-	destination: destMockInfo,
+	destination: undefined,
 	setDemoBuildingInfo: (building: Building) => set(() => ({ origin: building, destination: building })),
 	setOriginCoord: (lng: number, lat: number) => set(({ origin }) => ({ origin: { ...origin, lng, lat } })),
 	setDestinationCoord: (lng: number, lat: number) =>
