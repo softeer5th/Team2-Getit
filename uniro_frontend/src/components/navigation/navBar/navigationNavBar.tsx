51381ee3a227885af4a46c55bd68629c4454878e
JunhyukPark(박준혁) <peterp1@inha.edu>
2025-02-14 01:53:08 +0900

    [UNI-222] fix : 잘못된 record key 수정

--- uniro_frontend/src/components/navigation/navBar/navigationNavBar.tsx
+++ uniro_frontend/src/components/navigation/navBar/navigationNavBar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-import ElectricIcon from "../assets/route/detail/electric.svg?react";
-import WheelChairIcon from "../assets/route/detail/wheelchair.svg?react";
+import ElectricIcon from "../../../assets/route/detail/electric.svg?react";
+import WheelChairIcon from "../../../assets/route/detail/wheelchair.svg?react";
 import { NavigationButtonRouteType, NavigationRouteListRecord } from "../../../data/types/route";
 
 type Props = {
@@ -34,14 +34,14 @@ const calculateTotalTime = (
 		return {
 			pedestrian: convertSecondToMinute(route["PEDES & SAFE"].totalCost),
 			electric: convertSecondToMinute(route["ELECTRIC & CAUTION"].totalCost),
-			wheelchair: convertSecondToMinute(route["WHEELCHAIR & CAUTION"].totalCost),
+			wheelchair: convertSecondToMinute(route["MANUAL & CAUTION"].totalCost),
 		};
 	}
 	// dataLength === 3인 경우
 	return {
 		pedestrian: convertSecondToMinute(route["PEDES & SAFE"].totalCost),
-		electric: convertSecondToMinute(route["ELECTRIC & SAFE"].totalCost),
-		wheelchair: convertSecondToMinute(route["WHEELCHAIR & SAFE"].totalCost),
+		electric: convertSecondToMinute(route["ELECTRIC & CAUTION"].totalCost),
+		wheelchair: convertSecondToMinute(route["MANUAL & CAUTION"].totalCost),
 	};
 };
 
@@ -52,21 +52,21 @@ const NavigationNavBar = ({ route, dataLength, buttonType, setButtonState }: Pro
 		<div className="z-10 w-full flex flex-row items-center justify-start space-x-2 mt-2 px-2 overflow-x-scroll overflow-y-hidden whitespace-nowrap">
 			<div
 				onClick={() => setButtonState("PEDES & SAFE")}
-				className={`scroll-snap-x mandatory flex flex-row min-w-max items-center justify-center ${buttonType === "PEDES & SAFE" ? "bg-blue-600" : "bg-blue-300"} rounded-xl py-2 px-4 text-gray-100 text-kor-body3 font-light`}
+				className={`scroll-snap-x mandatory flex flex-row min-w-max items-center justify-center ${buttonType === "PEDES & SAFE" ? "bg-blue-600" : "bg-blue-300"} active:bg-blue-800 transition-colors rounded-xl py-2 px-4 text-gray-100 text-kor-body3 font-light`}
 			>
 				<ElectricIcon className="w-5 h-5 mr-1" />
 				{`도보 ${calculatedTimes.pedestrian}분`}
 			</div>
 			<div
 				onClick={() => setButtonState("ELECTRIC & CAUTION")}
-				className={`flex flex-row min-w-max items-center justify-center ${buttonType === "ELECTRIC & SAFE" || buttonType === "ELECTRIC & CAUTION" ? "bg-blue-600" : "bg-blue-300"} rounded-xl py-2 px-4 text-gray-100 text-kor-body3 font-light`}
+				className={`flex flex-row min-w-max items-center justify-center ${buttonType === "ELECTRIC & SAFE" || buttonType === "ELECTRIC & CAUTION" ? "bg-blue-600" : "bg-blue-300"} active:bg-blue-800 transition-colors rounded-xl py-2 px-4 text-gray-100 text-kor-body3 font-light`}
 			>
 				<ElectricIcon className="w-5 h-5 mr-1" />
 				{`전동 휠체어 ${calculatedTimes.electric}분`}
 			</div>
 			<div
-				onClick={() => setButtonState("WHEELCHAIR & CAUTION")}
-				className={`flex flex-row min-w-max items-center justify-center ${buttonType === "WHEELCHAIR & SAFE" || buttonType === "WHEELCHAIR & CAUTION" ? "bg-blue-600" : "bg-blue-300"} rounded-xl py-2 px-4 text-gray-100 text-kor-body3 font-light`}
+				onClick={() => setButtonState("MANUAL & CAUTION")}
+				className={`flex flex-row min-w-max items-center justify-center ${buttonType === "MANUAL & SAFE" || buttonType === "MANUAL & CAUTION" ? "bg-blue-600" : "bg-blue-300"} active:bg-blue-800 transition-colors rounded-xl py-2 px-4 text-gray-100 text-kor-body3 font-light`}
 			>
 				<WheelChairIcon className="w-5 h-5 mr-1" />
 				{`휠체어 ${calculatedTimes.wheelchair}분`}
