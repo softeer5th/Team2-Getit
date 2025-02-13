222138700a289dd627aaa072a00e48731b5b3cca
songhyeonseong <shsum7@gmail.com>
2025-02-13 21:13:42 +0900

    [UNI-194] chore : mock 데이터 제공을 위해 스웨거 desc 수정

--- uniro_backend/src/main/java/com/softeer5/uniro_backend/map/dto/response/FastestRouteResDTO.java
+++ uniro_backend/src/main/java/com/softeer5/uniro_backend/map/dto/response/FastestRouteResDTO.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Schema(name = "FastestRouteResDTO", description = "빠른 경로 조회 DTO")
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class FastestRouteResDTO {
-    @Schema(description = "길찾기 타입 (PEDES-도보, WHEEL_FAST-휠체어빠른, WHEEL_SAFE-휠체어안전)", example = "true")
+    @Schema(description = "길찾기 타입 (PEDES-도보, WHEEL_FAST-휠체어빠른, WHEEL_SAFE-휠체어안전)", example = "PEDES")
     private final RoadExclusionPolicy routeType;
     @Schema(description = "길 찾기 결과에 위험요소가 포함되어있는지 여부", example = "true")
     private final boolean hasCaution;
@@ -20,9 +20,9 @@ public class FastestRouteResDTO {
     private final double totalDistance;
     @Schema(description = "총 걸리는 시간(초) - 도보", example = "1050.32198432")
     private final Double pedestrianTotalCost;
-    @Schema(description = "총 걸리는 시간(초) - 수동휠체어", example = "1050.32198432")
+    @Schema(description = "총 걸리는 시간(초) - 수동휠체어", example = "2253.51234432")
     private final Double manualTotalCost;
-    @Schema(description = "총 걸리는 시간(초) - 전동휠체어", example = "1050.32198432")
+    @Schema(description = "총 걸리는 시간(초) - 전동휠체어", example = "935.3125632")
     private final Double electricTotalCost;
     @Schema(description = "길 찾기 결과에 포함된 모든 길", example = "")
     private final List<RouteInfoResDTO> routes;
