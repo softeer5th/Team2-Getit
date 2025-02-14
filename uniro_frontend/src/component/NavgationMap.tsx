441365d6e2895ecba0edb1b25b47ec2f29ebe64f
JunhyukPark(박준혁) <peterp1@inha.edu>
2025-02-15 04:34:58 +0900

    [UNI-222] feat : 정적 waypoint를 효율적으로 렌더링 할 수 있는 로직 작성

--- uniro_frontend/src/component/NavgationMap.tsx
+++ uniro_frontend/src/component/NavgationMap.tsx
@@ -187,28 +187,27 @@ const NavigationMap = ({
 		};
 	}, [map, buttonState, compositeRoutes, topPadding, bottomPadding]);
 
-	const createRiskMarkers =
-		(
-			riskData: DangerRoute[] | CautionRoute[],
-			map: google.maps.Map,
-			AdvancedMarker: typeof google.maps.marker.AdvancedMarkerElement,
-		): AdvancedMarker[] => {
-			const markers: AdvancedMarker[] = [];
-			riskData.forEach((route) => {
-				const marker = createAdvancedMarker(
-					AdvancedMarker,
-					map,
-					interpolate(route.node1, route.node2, 0.5),
-					createMarkerElement({
-						type: Markers.DANGER,
-						className: "translate-marker",
-						hasAnimation: true,
-					}),
-				);
-				markers.push(marker);
-			});
-			return markers;
-		}
+	const createRiskMarkers = (
+		riskData: DangerRoute[] | CautionRoute[],
+		map: google.maps.Map,
+		AdvancedMarker: typeof google.maps.marker.AdvancedMarkerElement,
+	): AdvancedMarker[] => {
+		const markers: AdvancedMarker[] = [];
+		riskData.forEach((route) => {
+			const marker = createAdvancedMarker(
+				AdvancedMarker,
+				map,
+				interpolate(route.node1, route.node2, 0.5),
+				createMarkerElement({
+					type: Markers.DANGER,
+					className: "translate-marker",
+					hasAnimation: true,
+				}),
+			);
+			markers.push(marker);
+		});
+		return markers;
+	};
 
 	const createStartEndMarkers = () => {
 		if (!map || !origin || !destination || !AdvancedMarker) return;
@@ -230,7 +229,7 @@ const NavigationMap = ({
 		});
 		const destinationMarker = createAdvancedMarker(AdvancedMarker, map, destination, destinationMarkerElement);
 		return [originMarker, destinationMarker];
-	}
+	};
 
 	useEffect(() => {
 		if (!map || !risks || !AdvancedMarker) return;
@@ -243,23 +242,60 @@ const NavigationMap = ({
 		routeData: NavigationRouteList,
 		map: google.maps.Map,
 		AdvancedMarker: typeof google.maps.marker.AdvancedMarkerElement,
+		prevMarkers?: AdvancedMarker[],
 	): AdvancedMarker[] => {
 		const markers: AdvancedMarker[] = [];
 		const bounds = new google.maps.LatLngBounds();
 
 		// 경로 간선 – 각 routeDetail에 대해 waypoint 마커 (주의 정보가 있으면 caution 마커)
-		routeData.routeDetails.forEach((routeDetail) => {
+		// 이전 마커와 현재 마커의 개수가 다르면 다른 길로 간주하고 마커를 다시 생성
+		if (prevMarkers?.length !== routeData.routeDetails.length) {
+			routeData.routeDetails.forEach((routeDetail) => {
+				const { coordinates } = routeDetail;
+				bounds.extend(coordinates);
+				let markerElement = createMarkerElement({
+					type: Markers.WAYPOINT,
+					className: "translate-waypoint",
+					hasAnimation: true,
+				});
+				if (routeDetail.cautionFactors && routeDetail.cautionFactors.length > 0) {
+					markerElement = createMarkerElement({
+						type: Markers.CAUTION,
+						className: "translate-routemarker",
+						hasAnimation: true,
+					});
+				}
+				const marker = createAdvancedMarker(AdvancedMarker, map, coordinates, markerElement);
+				markers.push(marker);
+			});
+			return markers;
+		}
+
+		// 마커 재사용 로직
+		routeData.routeDetails.forEach((routeDetail, index) => {
 			const { coordinates } = routeDetail;
 			bounds.extend(coordinates);
+
+			// 이전 좌표와 동일한 좌표이면 이전 마커를 그대로 사용
+			if (prevMarkers && prevMarkers[index]) {
+				const { position } = prevMarkers[index];
+				const { lat, lng } = position!;
+				if (Number(lat) === Number(coordinates.lat) && Number(lng) === Number(coordinates.lng)) {
+					const markerElement = prevMarkers[index];
+					markers.push(markerElement);
+				}
+				return;
+			}
 			let markerElement = createMarkerElement({
 				type: Markers.WAYPOINT,
 				className: "translate-waypoint",
 				hasAnimation: true,
 			});
+
 			if (routeDetail.cautionFactors && routeDetail.cautionFactors.length > 0) {
 				markerElement = createMarkerElement({
 					type: Markers.CAUTION,
-					className: "translate-marker",
+					className: "translate-routemarker",
 					hasAnimation: true,
 				});
 			}
@@ -274,28 +310,20 @@ const NavigationMap = ({
 	useEffect(() => {
 		if (!map || !origin || !destination || !AdvancedMarker) return;
 
-		const markers = createStaticMarkers(routeResult[buttonState], map, AdvancedMarker);
-
-		// 기존 마커 제거
-		staticMarkersRef.current.forEach(marker => {
-			const markerElement = marker.content as HTMLElement;
-			if (markerElement) {
-				markerElement.classList.add("fade-out");
-				setTimeout(() => {
-					marker.map = null;
-				}, 300);
-			} else {
-				marker.map = null;
+		const newMarkers = createStaticMarkers(routeResult[buttonState], map, AdvancedMarker, staticMarkersRef.current);
+		// 기존 마커 제거, includes로 새로운 마커가 기존 마커에 포함되어 있는지 확인
+		// 단점 : 마커가 길어지면 복잡해질 수 있지만, 깜빡임 현상을 방지할 수 있음
+		staticMarkersRef.current.forEach((oldMarker) => {
+			if (!newMarkers.includes(oldMarker)) {
+				oldMarker.map = null; // 지도에서 제거
 			}
 		});
 
 		staticMarkersRef.current = [];
-		staticMarkersRef.current = markers;
+		staticMarkersRef.current = newMarkers;
 
 		const currentRoute = routeResult[buttonState];
 		if (!currentRoute) return;
-
-
 	}, [map, buttonState, routeResult]);
 	// 동적 마커 그리는 부분
 	const drawDynamicMarker = (routeResult: NavigationRouteList) => {
@@ -309,7 +337,7 @@ const NavigationMap = ({
 				const markerElement = createMarkerElement({
 					type: Markers.NUMBERED_WAYPOINT,
 					number: index + 1,
-					hasAnimation: true
+					hasAnimation: true,
 				});
 
 				const marker = createAdvancedMarker(AdvancedMarker, map, coordinates, markerElement);
