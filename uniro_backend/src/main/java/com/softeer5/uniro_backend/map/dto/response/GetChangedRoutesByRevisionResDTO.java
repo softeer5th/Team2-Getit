ec7c4e16437d1e8cb9406affcb0b4b7bbc324fbf
songhyeonseong <shsum7@gmail.com>
2025-02-15 14:44:27 +0900

    [UNI-238] feat : 현재 버전과 특정 버전의 차이점 조회 로직 DTO 정의

---
+++ uniro_backend/src/main/java/com/softeer5/uniro_backend/map/dto/response/GetChangedRoutesByRevisionResDTO.java
@@ -0,0 +1,24 @@
+package com.softeer5.uniro_backend.map.dto.response;
+
+import com.softeer5.uniro_backend.admin.dto.response.ChangedRouteDTO;
+import com.softeer5.uniro_backend.admin.dto.response.LostRoutesDTO;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@Schema(name = "GetChangedRoutesByRevisionResDTO", description = "현재 버전과 특정 버전의 차이점 조회 응답 DTO")
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public class GetChangedRoutesByRevisionResDTO {
+    @Schema(description = "삭제된 길&노드 정보 정보", example = "")
+    private final LostRoutesDTO lostRoutes;
+    @Schema(description = "현재 버전과 비교하여 변경된 주의/위험 요소 정보", example = "")
+    private final List<ChangedRouteDTO> changedList;
+
+    public static GetChangedRoutesByRevisionResDTO of(LostRoutesDTO lostRoutes, List<ChangedRouteDTO> changedList) {
+        return new GetChangedRoutesByRevisionResDTO(lostRoutes,changedList);
+    }
+}
