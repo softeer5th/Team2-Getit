007de957e0524bbc5c29e1a5a5222320b195ab74
mikekks <thdalsrb79@naver.com>
2025-02-15 15:52:38 +0900

    [UNI-244] feat: 대학교 관련 데이터 반환 기능 구현

--- uniro_backend/src/main/java/com/softeer5/uniro_backend/univ/repository/UnivCustomRepository.java
+++ uniro_backend/src/main/java/com/softeer5/uniro_backend/univ/repository/UnivCustomRepository.java
@@ -1,9 +1,9 @@
 package com.softeer5.uniro_backend.univ.repository;
 
-import com.softeer5.uniro_backend.univ.dto.UnivInfo;
+import com.softeer5.uniro_backend.univ.entity.Univ;
 
 import java.util.List;
 
 public interface UnivCustomRepository {
-    List<UnivInfo> searchUniv(String name, Integer pageSize);
+    List<Univ> searchUniv(String name, Integer pageSize);
 }
