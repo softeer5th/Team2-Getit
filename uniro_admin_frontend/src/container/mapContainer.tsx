ded6cff77cb3c7690168c51901d51b62a1b35f77
dgfh0450 <dgfh0450@gmail.com>
2025-02-18 18:13:55 +0900

    [UNI-229] refactor : 모달 컴포넌트 분리

--- uniro_admin_frontend/src/container/mapContainer.tsx
+++ uniro_admin_frontend/src/container/mapContainer.tsx
@@ -5,7 +5,9 @@ import { patchRevision } from "../api/admin";
 import useLogin from "../hooks/useLogin";
 import LogMap from "../component/LogMap";
 import { useEffect, useState } from "react";
-import { CautionIssue, DangerIssue } from "../constant/enum/reportEnum";
+import RollbackModal from "../components/log/rollbackModal";
+import FailModal from "../components/log/failModal";
+import InfoModal from "../components/log/infoModal";
 
 interface MapContainerProps {
 	rev: RevisionType;
@@ -27,7 +29,7 @@ const MapContainer = ({ rev, data }: MapContainerProps) => {
 	const [infoModalOpen, setInfoModalOpen] = useState<boolean>(false);
 	const [info, setInfo] = useState<ChangedInfo>();
 
-	const { status, isSuccess, mutate } = useMutation({
+	const { mutate } = useMutation({
 		mutationFn: () => patchRevision(accessToken, university?.id ?? -1, data?.rev ?? -1),
 		onSuccess: () => {
 			closeModal();
@@ -80,63 +82,15 @@ const MapContainer = ({ rev, data }: MapContainerProps) => {
 			<LogMap revisionData={data} center={university?.centerPoint} setInfo={setInfo} />
 			{
 				modalOpen &&
-				<div className="w-screen h-screen absolute top-0 left-0 flex items-center justify-center bg-[rgba(0,0,0,0.5)]">
-					<div className="w-[300px] h-[300px] bg-gray-100 rounded-200 p-5 space-y-4 flex flex-col justify-center">
-						<h2 className="font-bold text-xl "><i>{data?.rev}</i> 로 되돌리시겠습니까?</h2>
-						<p className="font-semibold">되돌리기 작업은 취소될 수 없습니다.</p>
-						<div className="w-full flex justify-around ">
-							<button className="w-[100px] h-[30px] border border-gray-400 rounded-100 cursor-pointer text-primary-500" onClick={() => mutate()}>확인</button>
-							<button className="w-[100px] h-[30px] border border-gray-400 rounded-100 cursor-pointer text-system-red" onClick={closeModal}>취소</button>
-						</div>
-					</div>
-				</div>
+				<RollbackModal rev={data!.rev} onRollback={() => mutate()} onClose={closeModal} />
 			}
 			{
 				failModalOpen &&
-				<div className="w-screen h-screen absolute top-0 left-0 flex items-center justify-center bg-[rgba(0,0,0,0.5)]">
-					<div className="w-[300px] h-[300px] bg-gray-100 rounded-200 p-5 space-y-4 flex flex-col justify-center">
-						<h2 className="font-bold text-xl text-system-red ">에러가 발생하였습니다.</h2>
-						<p className="font-semibold">관리자에게 문의바랍니다.</p>
-						<div className="w-full flex justify-around ">
-							<button className="w-[100px] h-[30px] border border-gray-400 rounded-100 cursor-pointer text-system-red" onClick={closeFailModal}>확인</button>
-						</div>
-					</div>
-				</div>
+				<FailModal onClose={closeFailModal} />
 			}
 			{
 				infoModalOpen &&
-				<div className="w-screen h-screen absolute top-0 left-0 flex items-center justify-center bg-[rgba(0,0,0,0.5)]">
-					<div className="w-[500px] h-[400px] bg-gray-100 rounded-200 p-5 space-y-4 flex flex-col justify-center">
-						<h2 className="font-bold text-xl text-primary-500 ">위험 주의 요소 변경사항.</h2>
-						<div className="flex flex-row flex-1">
-							<div className="w-1/2 space-y-5 border-r-1 border-gray-400">
-								<h2 className="text-lg font-bold">이전 요소</h2>
-								<div>
-									<h3 className="text-base font-semibold text-system-orange">주의 요소</h3>
-									{info!.current.cautionFactors.map(el => <p className="text-sm">{CautionIssue[el]}</p>)}
-								</div>
-								<div>
-									<h3 className="text-base font-semibold text-system-red">위험 요소</h3>
-									{info!.current.dangerFactors.map(el => <p className="text-sm">{DangerIssue[el]}</p>)}
-								</div>
-							</div>
-							<div className="w-1/2 space-y-5 ">
-								<h2 className="text-lg font-bold">현재 요소</h2>
-								<div>
-									<h3 className="text-base font-semibold text-system-orange">주의 요소</h3>
-									{info!.difference.cautionFactors.map(el => <p className="text-sm">{CautionIssue[el]}</p>)}
-								</div>
-								<div>
-									<h3 className="text-base font-semibold text-system-red">위험 요소</h3>
-									{info!.difference.dangerFactors.map(el => <p className="text-sm">{DangerIssue[el]}</p>)}
-								</div>
-							</div>
-						</div>
-						<div className="w-full flex justify-around ">
-							<button className="w-[100px] h-[30px] border border-gray-400 rounded-100 cursor-pointer text-system-red" onClick={closeInfoModal}>확인</button>
-						</div>
-					</div>
-				</div>
+				<InfoModal info={info!} onClose={closeInfoModal} />
 			}
 		</div>
 	);
