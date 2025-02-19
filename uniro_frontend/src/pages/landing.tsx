7358337ef60bbdeb3d8e16eaf3051368e820e3d4
JunhyukPark(박준혁) <peterp1@inha.edu>
2025-02-19 14:15:06 +0900

    [UNI-308] feat : 랜딩 페이지 애니메이션

--- uniro_frontend/src/pages/landing.tsx
+++ uniro_frontend/src/pages/landing.tsx
@@ -5,6 +5,7 @@ import { Link } from "react-router";
 import useRoutePoint from "../hooks/useRoutePoint";
 import useUniversityInfo from "../hooks/useUniversityInfo";
 import { useEffect } from "react";
+import { motion } from "framer-motion";
 
 export default function LandingPage() {
 	const { setUniversity } = useUniversityInfo();
@@ -22,10 +23,22 @@ export default function LandingPage() {
 
 	return (
 		<div className="relative flex flex-col h-dvh w-full max-w-[450px] mx-auto justify-center bg-[url(/public/background.png)] bg-cover">
-			<div>
-				<p className="text-kor-heading1 font-bold">어디든 갈 수 있는 캠퍼스.</p>
-				<p className="text-kor-heading1 font-bold">쉽고 빠르게 이동하세요.</p>
-			</div>
+			<motion.p
+				initial={{ opacity: 0, y: 50 }}
+				animate={{ opacity: 1, y: 0 }}
+				transition={{ duration: 0.5, delay: 0.3, ease: "easeInOut" }}
+				className="text-kor-heading1 font-bold"
+			>
+				어디든 갈 수 있는 캠퍼스.
+			</motion.p>
+			<motion.p
+				initial={{ opacity: 0, y: 50 }}
+				animate={{ opacity: 1, y: 0 }}
+				transition={{ duration: 0.5, delay: 0.8, ease: "easeInOut" }}
+				className="text-kor-heading1 font-bold"
+			>
+				쉽고 빠르게 이동하세요.
+			</motion.p>
 			<div className="w-full absolute bottom-6 left-[50%] px-[14px] translate-x-[-50%] flex flex-col items-start">
 				<div className="flex flex-row items-center space-x-[7px] mb-5">
 					<Logo />
