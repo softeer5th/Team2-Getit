5fdecc760cce864edea433dd7bdbb5a905296129
JunhyukPark(박준혁) <peterp1@inha.edu>
2025-02-19 14:31:02 +0900

    [UNI-311] feat : Skeleton UI 적용

--- uniro_frontend/src/components/universityButton.tsx
+++ uniro_frontend/src/components/universityButton.tsx
@@ -1,13 +1,15 @@
-import { MouseEvent } from "react";
+import { MouseEvent, useState } from "react";
 
 interface UniversityButtonProps {
 	name: string;
 	img: string;
 	selected: boolean;
+	loading?: boolean;
 	onClick: () => void;
 }
 
-export default function UniversityButton({ name, img, selected, onClick }: UniversityButtonProps) {
+export default function UniversityButton({ name, img, selected, loading, onClick }: UniversityButtonProps) {
+	const [imageLoaded, setImageLoaded] = useState(false);
 	const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
 		e.stopPropagation();
 		onClick();
@@ -17,9 +19,19 @@ export default function UniversityButton({ name, img, selected, onClick }: Unive
 		<li className="my-[6px]">
 			<button
 				onClick={handleClick}
-				className={`w-full h-full p-6 flex flex-row items-center border rounded-400 ${selected ? "border-primary-400 bg-system-skyblue text-primary-500" : "border-gray-400"} `}
+				className={`w-full h-full p-6 flex flex-row items-center border rounded-400 ${
+					selected ? "border-primary-400 bg-system-skyblue text-primary-500" : "border-gray-400"
+				}`}
 			>
-				<img src={img} className="mr-4" />
+				{!imageLoaded && <div className="w-[30px] h-[30px] bg-gray-300 rounded-full  animate-pulse" />}
+				<img
+					src={img}
+					className={`mr-4 transition-opacity duration-300 ${imageLoaded ? "opacity-100" : "opacity-0"}`}
+					alt={`${name} 로고`}
+					onLoad={() => setImageLoaded(true)}
+					onError={() => setImageLoaded(true)} // 에러 발생 시에도 로딩 완료로 간주
+				/>
+
 				<span className="text-kor-body2 font-medium leading-[140%]">{name}</span>
 			</button>
 		</li>
