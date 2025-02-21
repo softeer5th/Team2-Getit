7317f13c9e0f5858771bc06e3ac561bd3385a413
JunhyukPark(박준혁) <peterp1@inha.edu>
2025-02-21 10:32:59 +0900

    [UNI-345] feat: 건물 클릭 시 해당으로 이동 구현

--- uniro_frontend/src/component/NavgationMap.tsx
+++ uniro_frontend/src/component/NavgationMap.tsx
@@ -69,6 +69,13 @@ const NavigationMap = ({
 	currentRouteIdx,
 	handleCautionMarkerClick,
 }: MapProps) => {
+	const buttonStateRef = useRef(buttonState);
+
+	// Listener들이 항상 최신 buttonStateRef를 참조하도록 ref 사용
+	useEffect(() => {
+		buttonStateRef.current = buttonState;
+	}, [buttonState]);
+
 	const { mapRef, map, AdvancedMarker, Polyline } = useMap();
 	const { origin, destination } = useRoutePoint();
 
@@ -200,7 +207,9 @@ const NavigationMap = ({
 			hasAnimation: true,
 		});
 		const originMarker = createAdvancedMarker(AdvancedMarker, map, origin, originMarkerElement);
-
+		originMarker.addListener("click", () => {
+			handleCautionMarkerClick(0);
+		});
 		// 도착지 마커
 		const destinationMarkerElement = createMarkerElement({
 			type: Markers.DESTINATION,
@@ -209,7 +218,9 @@ const NavigationMap = ({
 			hasAnimation: true,
 		});
 		const destinationMarker = createAdvancedMarker(AdvancedMarker, map, destination, destinationMarkerElement);
-
+		destinationMarker.addListener("click", () => {
+			handleCautionMarkerClick(routeResult[buttonStateRef.current].routeDetails.length);
+		});
 		// bounds 업데이트
 		boundsRef.current?.extend(origin);
 		boundsRef.current?.extend(destination);
