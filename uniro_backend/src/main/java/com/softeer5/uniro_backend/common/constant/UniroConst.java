81cdd636f57e6f53304ec876347938c26327d03c
songhyeonseong <shsum7@gmail.com>
2025-02-22 01:27:30 +0900

    [UNI-354] refactor : 가중치 조정

--- uniro_backend/src/main/java/com/softeer5/uniro_backend/common/constant/UniroConst.java
+++ uniro_backend/src/main/java/com/softeer5/uniro_backend/common/constant/UniroConst.java
@@ -5,11 +5,11 @@ public final class UniroConst {
 	public static final int CORE_NODE_CONDITION = 3;
 	public static final int BEFORE_DEFAULT_ORDER = -1;
 	public static final double PEDESTRIAN_SECONDS_PER_MITER = 1.0;
-	public static final double MANUAL_WHEELCHAIR_SECONDS_PER_MITER = 2.0;
+	public static final double MANUAL_WHEELCHAIR_SECONDS_PER_MITER = 2.1;
 	public static final double ELECTRIC_WHEELCHAIR_SECONDS_PER_MITER = 0.9;
 	public static final double EARTH_RADIUS = 6378137;
 	public static final double BUILDING_ROUTE_DISTANCE = 1e8;
-	public static final int IS_SINGLE_ROUTE = 2;
+	public static final double LIMIT_RANGE = 500;
 	public static final double HEURISTIC_WEIGHT_NORMALIZATION_FACTOR = 10.0;
 	public static final int STREAM_FETCH_SIZE = 2500;
 	//컴파일 상수를 보장하기 위한 코드
