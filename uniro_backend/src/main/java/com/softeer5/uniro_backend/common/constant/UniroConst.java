606f70a3735d04de4b154bdc78adc78bdc0095ef
songhyeonseong <shsum7@gmail.com>
2025-02-21 23:44:26 +0900

    [UNI-354] refactor : 가중치 재조정

--- uniro_backend/src/main/java/com/softeer5/uniro_backend/common/constant/UniroConst.java
+++ uniro_backend/src/main/java/com/softeer5/uniro_backend/common/constant/UniroConst.java
@@ -4,9 +4,9 @@ public final class UniroConst {
 	public static final String NODE_KEY_DELIMITER = " ";
 	public static final int CORE_NODE_CONDITION = 3;
 	public static final int BEFORE_DEFAULT_ORDER = -1;
-	public static final double PEDESTRIAN_SECONDS_PER_MITER = 1.2;
-	public static final double MANUAL_WHEELCHAIR_SECONDS_PER_MITER = 2.5;
-	public static final double ELECTRIC_WHEELCHAIR_SECONDS_PER_MITER = 1.0;
+	public static final double PEDESTRIAN_SECONDS_PER_MITER = 1.0;
+	public static final double MANUAL_WHEELCHAIR_SECONDS_PER_MITER = 2.0;
+	public static final double ELECTRIC_WHEELCHAIR_SECONDS_PER_MITER = 0.9;
 	public static final double EARTH_RADIUS = 6378137;
 	public static final double BUILDING_ROUTE_DISTANCE = 1e8;
 	public static final int IS_SINGLE_ROUTE = 2;
