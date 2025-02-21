9c62fe524ab7091e62762225466eb68f535c830d
JunhyukPark(박준혁) <peterp1@inha.edu>
2025-02-22 00:00:42 +0900

    [UNI-312] feat : uniro admin에 brotli 압축 적용

--- uniro_admin_frontend/vite.config.ts
+++ uniro_admin_frontend/vite.config.ts
@@ -2,8 +2,23 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import svgr from "vite-plugin-svgr";
+import compression from "vite-plugin-compression";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr()],
+	plugins: [
+		react(),
+		tailwindcss(),
+		svgr(),
+		compression({
+			algorithm: "brotliCompress",
+			ext: ".br",
+			threshold: 1024,
+			deleteOriginFile: false,
+		}),
+		compression({
+			algorithm: "gzip",
+			ext: ".gz",
+		}),
+	],
 });
