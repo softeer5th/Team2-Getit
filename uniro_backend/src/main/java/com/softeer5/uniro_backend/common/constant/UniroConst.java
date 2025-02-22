23a3cedca78bfde9dcdb2f55629801b675b0426d
mikekks <thdalsrb79@naver.com>
2025-02-22 19:40:47 +0900

    [UNI-255] refactor: 배치사이즈 조정

--- uniro_backend/src/main/java/com/softeer5/uniro_backend/common/constant/UniroConst.java
+++ uniro_backend/src/main/java/com/softeer5/uniro_backend/common/constant/UniroConst.java
@@ -11,7 +11,7 @@ public final class UniroConst {
 	public static final double BUILDING_ROUTE_DISTANCE = 1e8;
 	public static final int IS_SINGLE_ROUTE = 2;
 	public static final double HEURISTIC_WEIGHT_NORMALIZATION_FACTOR = 10.0;
-	public static final int STREAM_FETCH_SIZE = 2500;
+	public static final int STREAM_FETCH_SIZE = 250;
 	//컴파일 상수를 보장하기 위한 코드
 	public static final String STREAM_FETCH_SIZE_AS_STRING = "" + STREAM_FETCH_SIZE;
 
