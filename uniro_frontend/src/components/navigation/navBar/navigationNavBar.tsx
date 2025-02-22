c7e1802cf8e18abbb47b9a55d4ff2e5d472c1e49
JunhyukPark(박준혁) <peterp1@inha.edu>
2025-02-22 16:53:29 +0900

    [UNI-363] fix : 조회 순서 변경

--- uniro_frontend/src/components/navigation/navBar/navigationNavBar.tsx
+++ uniro_frontend/src/components/navigation/navBar/navigationNavBar.tsx
@@ -52,13 +52,6 @@ const NavigationNavBar = ({ route, dataLength, buttonType, setButtonState }: Pro
 
 	return (
 		<div className="z-10 w-full flex flex-row items-center justify-start space-x-2 mt-2 px-2 overflow-x-scroll overflow-y-hidden whitespace-nowrap">
-			<div
-				onClick={() => setButtonState("PEDES & SAFE")}
-				className={`scroll-snap-x mandatory flex flex-row min-w-max items-center justify-center ${buttonType === "PEDES & SAFE" ? "bg-blue-600" : "bg-blue-300"} active:bg-blue-800 transition-colors rounded-xl py-2 px-4 text-gray-100 text-kor-body3 font-light`}
-			>
-				<ElectricIcon className="w-5 h-5 mr-1" />
-				{`도보 ${calculatedTimes.pedestrian}분`}
-			</div>
 			{(keyExists("ELECTRIC & CAUTION") ||
 				keyExists("MANUAL & CAUTION") ||
 				keyExists("ELECTRIC & SAFE") ||
@@ -88,6 +81,13 @@ const NavigationNavBar = ({ route, dataLength, buttonType, setButtonState }: Pro
 					</div>
 				</>
 			)}
+			<div
+				onClick={() => setButtonState("PEDES & SAFE")}
+				className={`scroll-snap-x mandatory flex flex-row min-w-max items-center justify-center ${buttonType === "PEDES & SAFE" ? "bg-blue-600" : "bg-blue-300"} active:bg-blue-800 transition-colors rounded-xl py-2 px-4 text-gray-100 text-kor-body3 font-light`}
+			>
+				<ElectricIcon className="w-5 h-5 mr-1" />
+				{`도보 ${calculatedTimes.pedestrian}분`}
+			</div>
 		</div>
 	);
 };
