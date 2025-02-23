c7b796766ec906a0f713566a2bd6462647d46d2a
mikekks <thdalsrb79@naver.com>
2025-02-24 00:00:58 +0900

    [UNI-199] fix: 로컬 캐시 삭제

--- uniro_backend/src/main/java/com/softeer5/uniro_backend/external/redis/RedisService.java
+++ uniro_backend/src/main/java/com/softeer5/uniro_backend/external/redis/RedisService.java
@@ -1,11 +1,9 @@
 package com.softeer5.uniro_backend.external.redis;
 
 import java.time.Duration;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
@@ -17,37 +15,25 @@ import lombok.extern.slf4j.Slf4j;
 public class RedisService {
 
 	private final RedisTemplate redisTemplate;
-	private final Map<String, Boolean> cacheMap = new HashMap<>();
+	private final StringRedisTemplate stringRedisTemplate;
 
 	public void saveData(String key, Object value) {
-		long startTime = System.nanoTime();
-		redisTemplate.opsForValue().set(key, value, Duration.ofMinutes(1000)); // 10분 TTL
-		long endTime = System.nanoTime();
+		redisTemplate.opsForValue().set(key, value, Duration.ofMinutes(10000));
+	}
 
-		long duration = TimeUnit.NANOSECONDS.toMicros(endTime - startTime);
-		log.info("🔴🔴🔴🔴🔴🔴🔴 Redis 직렬화 및 저장 시간: {} 마이크로초", duration);
-
-		cacheMap.put(key, true);
+	public void saveDataToString(String key, Object value) {
+		stringRedisTemplate.opsForValue().set(key, value.toString(), Duration.ofMinutes(10000));
 	}
 
 	public Object getData(String key) {
-		long startTime = System.nanoTime();
-		Object data = redisTemplate.opsForValue().get(key);
-		long endTime = System.nanoTime();
-
-		long duration = TimeUnit.NANOSECONDS.toMicros(endTime - startTime);
-		log.info("🟢🟢🟢🟢🟢🟢🟢 Redis 역직렬화 및 조회 시간: {} 마이크로초", duration);
-		cacheMap.put(key, true);
-		return data;
+		return redisTemplate.opsForValue().get(key);
 	}
 
+	public String getDataToString(String key){
+		return stringRedisTemplate.opsForValue().get(key);
+	}
 	public boolean hasData(String key){
-		return cacheMap.containsKey(key);
-	}
-
-	public void deleteData(String key) {
-		redisTemplate.delete(key);
-		cacheMap.remove(key);
+		return redisTemplate.hasKey(key);
 	}
 
 	public void deleteRoutesData(String key, int batchNumber) {
@@ -56,6 +42,5 @@ public class RedisService {
 		for(int i=1; i<=batchNumber; i++){
 			redisTemplate.delete(redisKeyPrefix + i);
 		}
-		cacheMap.remove(key);
 	}
 }
