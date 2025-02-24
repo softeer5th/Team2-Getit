beaf9a480664a24fcb47a62aebaec52e6618d2f5
YoonDH <114418121+dgfh0450@users.noreply.github.com>
2025-02-24 11:59:42 +0900

    [UNI-370] feat : 검색 리스트 로딩 + NotFound 화면 적용 (#226)

--- uniro_frontend/src/pages/universitySearch.tsx
+++ uniro_frontend/src/pages/universitySearch.tsx
@@ -1,25 +1,18 @@
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import Input from "../components/customInput";
-import UniversityButton from "../components/universityButton";
 import Button from "../components/customButton";
-import { Link, useNavigate } from "react-router";
+import { Link } from "react-router";
 import useUniversityInfo from "../hooks/useUniversityInfo";
-import { useQuery } from "@tanstack/react-query";
-import { getUniversityList } from "../api/search";
 import { University } from "../data/types/university";
 import useRoutePoint from "../hooks/useRoutePoint";
+import UniversityList from "../components/university/universityList";
+import InnerLoading from "../components/loading/innerLoading";
 
 export default function UniversitySearchPage() {
 	const [selectedUniv, setSelectedUniv] = useState<University>();
 	const { university, setUniversity } = useUniversityInfo();
 	const { setDestination, setOrigin } = useRoutePoint();
 	const [input, setInput] = useState<string>("");
-	const navigation = useNavigate();
-
-	const { data: universityList } = useQuery({
-		queryKey: ["university", input],
-		queryFn: () => getUniversityList(input),
-	});
 
 	useEffect(() => {
 		if (university) {
@@ -38,37 +31,14 @@ export default function UniversitySearchPage() {
 		<div className="h-full w-full" onClick={() => setSelectedUniv(undefined)}>
 			<div className="relative flex flex-col h-dvh w-full max-w-[450px] mx-auto py-5">
 				<div className="w-full px-[14px] pb-[17px] border-b-[1px] border-gray-400">
-					<Input
-						onChangeDebounce={(e) => setInput(e)}
-						placeholder="우리 학교를 검색해보세요"
-						handleVoiceInput={() => {}}
-					/>
+					<Input onChangeDebounce={(e) => setInput(e)} placeholder="우리 학교를 검색해보세요" />
 				</div>
 				<div className="overflow-y-scroll flex-1">
-					<ul
-						className="w-full h-full px-[14px] py-[6px]"
-						onClick={() => {
-							setSelectedUniv(undefined);
-						}}
+					<Suspense
+						fallback={<InnerLoading isLoading={true} loadingContent="학교 목록을 불러오고 있습니다." />}
 					>
-						{universityList &&
-							universityList.map((univ) => (
-								<UniversityButton
-									key={`university-${univ.id}`}
-									selected={selectedUniv?.id === univ.id}
-									onClick={() => {
-										if (selectedUniv?.id === univ.id) {
-											setUniversity(univ);
-											navigation("/map");
-											return;
-										}
-										setSelectedUniv(univ);
-									}}
-									name={univ.name}
-									img={univ.imageUrl}
-								/>
-							))}
-					</ul>
+						<UniversityList query={input} selectedUniv={selectedUniv} setSelectedUniv={setSelectedUniv} />
+					</Suspense>
 				</div>
 				<div className="px-[14px]">
 					{selectedUniv && (
