60bec6bc38dbc1aadeb4429111bb1ca318e203b2
dgfh0450 <dgfh0450@gmail.com>
2025-02-24 12:13:07 +0900

    [UNI-367] fix : SSE 종료 시점 하드코딩 변수 제거

--- uniro_frontend/src/pages/mapSSE.tsx
+++ uniro_frontend/src/pages/mapSSE.tsx
@@ -762,14 +762,10 @@ export default function MapSSEPage() {
 				const routes = transformAllRoutes({ coreRoutes, nodeInfos });
 				drawRoute(routes);
 				sseCounter.current += 1;
-				if (sseCounter.current === 11) {
+				if (sseCounter.current === batchSize) {
 					eventSource.close();
 				}
 			}
-		};
-
-		eventSource.onerror = (err) => {
-			console.log("ERROR");
 		};
 
 		return () => {
